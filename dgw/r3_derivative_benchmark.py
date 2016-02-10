e32044a6b799d3c6c0ff3bb727a29244756089ec
David Waterman <dgwaterman@gmail.com>
2016-02-10 15:44:28 +0000

    C++ version (in wrapper that returns a matrix.sqr) about 2* faster than the fastest Python version

--- dgw/r3_derivative_benchmark.py
+++ dgw/r3_derivative_benchmark.py
@@ -5,11 +5,15 @@ matrix.rec.axis_and_angle_as_r3_derivative_wrt_angle.
 '''
 
 from scitbx import matrix
+from scitbx.array_family import flex #required dependency
 import random
 from math import pi
 from libtbx.test_utils import approx_equal
 from libtbx.development.timers import Timer
-from dials.algorithms.refinement.refinement_helpers import dR_from_axis_and_angle
+from dials.algorithms.refinement.refinement_helpers import \
+  dR_from_axis_and_angle as dR_from_axis_and_angle_cpp
+from dials.algorithms.refinement.refinement_helpers import \
+  dR_from_axis_and_angle_py
 
 trials = []
 
@@ -18,10 +22,18 @@ for i in range(10000):
   trials.append((matrix.col.random(3, -1, 1).normalize(),
                  random.uniform(0, 2*pi)))
 
-t = Timer('dR_from_axis_and_angle')
+t = Timer('dR_from_axis_and_angle in C++')
+dR_0 = []
+for trial in trials:
+  dR_0.append(dR_from_axis_and_angle_cpp(trial[0], trial[1]))
+del(t)
+
+print
+
+t = Timer('dR_from_axis_and_angle in Python')
 dR_1 = []
 for trial in trials:
-  dR_1.append(dR_from_axis_and_angle(trial[0], trial[1]))
+  dR_1.append(dR_from_axis_and_angle_py(trial[0], trial[1]))
 del(t)
 
 print
