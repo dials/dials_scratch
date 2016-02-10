de184631cf5693d58a7f4fa7da70b1eca07eba2b
David Waterman <dgwaterman@gmail.com>
2016-02-10 11:42:06 +0000

    Benchmarking two python functions. In practice, should move to using a C++ version of dR_from_axis_and_angle

---
+++ dgw/r3_derivative_benchmark.py
@@ -0,0 +1,39 @@
+'''Comparison of execution times for two functions that do the same thing.
+
+Result: dR_from_axis_and_angle is approximately 3 times faster than
+matrix.rec.axis_and_angle_as_r3_derivative_wrt_angle.
+'''
+
+from scitbx import matrix
+import random
+from math import pi
+from libtbx.test_utils import approx_equal
+
+trials = []
+
+for i in range(10000):
+  # generate random axis and angle
+  trials.append((matrix.col.random(3, -1, 1).normalize(),
+                 random.uniform(0, 2*pi)))
+
+from libtbx.development.timers import Timer
+from dials.algorithms.refinement.refinement_helpers import dR_from_axis_and_angle
+
+t = Timer('dR_from_axis_and_angle')
+dR_1 = []
+for trial in trials:
+  dR_1.append(dR_from_axis_and_angle(trial[0], trial[1]))
+del(t)
+
+print
+
+t = Timer('axis_and_angle_as_r3_derivative_wrt_angle')
+dR_2 = []
+for trial in trials:
+  dR_2.append(trial[0].axis_and_angle_as_r3_derivative_wrt_angle(trial[1]))
+del (t)
+
+print "check results are the same"
+for (a, b) in zip(dR_1, dR_2):
+  assert approx_equal(a, b)
+print "OK"
