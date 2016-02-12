27d9ae991166d1c578a81b7f843b6085460417ba
rjgildea <richard.gildea@diamond.ac.uk>
2016-02-12 09:26:57 +0000

    fast/slow directions different for portrait/landscape grids (at DLS); only plot n_indexed related plots if some indexing results

--- rjg/plot_find_spots_client.py
+++ rjg/plot_find_spots_client.py
@@ -13,6 +13,7 @@ grid = None
 
 def run(args):
   from dials.util.options import OptionParser
+  from scitbx.array_family import flex
   import libtbx.load_env
 
   usage = "%s [options] find_spots.json" %(
@@ -33,10 +34,10 @@ def run(args):
   with open(json_file, 'rb') as f:
     results = json.load(f)
 
-  n_indexed = []
-  fraction_indexed = []
-  n_spots = []
-  n_lattices = []
+  n_indexed = flex.double()
+  fraction_indexed = flex.double()
+  n_spots = flex.double()
+  n_lattices = flex.double()
 
   for r in results:
     n_spots.append(r['n_spots_total'])
@@ -64,7 +65,6 @@ def run(args):
   table = True
   grid = params.grid
 
-  from scitbx.array_family import flex
   from libtbx import group_args
   from dials.algorithms.peak_finding.per_image_analysis \
        import plot_stats, print_table
@@ -99,68 +99,92 @@ def run(args):
   if table:
     print_table(stats)
 
-  def plot_grid(values, grid, file_name):
-    values = values.deep_copy()
-    values.reshape(flex.grid(grid))
+  def plot_grid(values, grid, file_name, cmap=pyplot.cm.Reds,
+                vmin=None, vmax=None):
+    values = values.as_double()
+    # At DLS, fast direction appears to be largest direction
+    if grid[0] > grid[1]:
+      values.reshape(flex.grid(reversed(grid)))
+      values = values.matrix_transpose()
+    else:
+      values.reshape(flex.grid(grid))
 
     fig = pyplot.figure()
-    pyplot.pcolormesh(values.as_numpy_array(), cmap=pyplot.cm.Reds)
+    pyplot.pcolormesh(
+      values.as_numpy_array(), cmap=cmap, vmin=vmin, vmax=vmax)
+    pyplot.xlim(0, grid[1])
+    pyplot.ylim(0, grid[0])
+    pyplot.gca().invert_yaxis()
     pyplot.axes().set_aspect('equal')
+    pyplot.colorbar()
     pyplot.savefig(file_name)
     pyplot.clf()
 
-
   if grid is not None:
     grid = tuple(reversed(grid))
+    plot_grid(n_spots_total, grid, 'grid_spot_count_total.png')
     plot_grid(n_spots_no_ice, grid, 'grid_spot_count_no_ice.png')
     plot_grid(total_intensity, grid, 'grid_total_intensity.png')
+    if flex.max(n_indexed) > 0:
+      plot_grid(n_indexed, grid, 'grid_n_indexed.png')
+      plot_grid(fraction_indexed, grid, 'grid_fraction_indexed.png')
 
     for i, d_min in enumerate((estimated_d_min, d_min_distl_method_1, d_min_distl_method_2)):
       from cctbx import uctbx
       d_star_sq = uctbx.d_as_d_star_sq(d_min)
       d_star_sq.set_selected(d_star_sq == 1, 0)
+      vmin = flex.min(d_star_sq.select(d_star_sq > 0))
+      vmax = flex.max(d_star_sq)
+
+      vmin = flex.min(d_min.select(d_min > 0))
+      vmax = flex.max(d_min)
+      cmap = pyplot.cm.Reds_r
+      d_min.set_selected(d_min <= 0, vmax)
+
       if i == 0:
-        plot_grid(d_star_sq, grid, 'grid_d_min.png')
+        plot_grid(d_min, grid, 'grid_d_min.png', cmap=cmap, vmin=vmin, vmax=vmax)
       else:
-        plot_grid(d_star_sq, grid, 'grid_d_min_method_%i.png' %i)
+        plot_grid(
+          d_min, grid, 'grid_d_min_method_%i.png' %i, cmap=cmap, vmin=vmin, vmax=vmax)
 
-  pyplot.scatter(n_spots, n_indexed, marker=marker, alpha=alpha, c=blue, lw=lw)
-  xlim = pyplot.xlim()
-  ylim = pyplot.ylim()
-  pyplot.plot([0, max(n_spots)], [0, max(n_spots)], c=red)
-  pyplot.xlim(0, xlim[1])
-  pyplot.ylim(0, ylim[1])
-  pyplot.xlabel('# spots')
-  pyplot.ylabel('# indexed')
-  pyplot.savefig('n_spots_vs_n_indexed.png')
-  pyplot.clf()
+  if flex.max(n_indexed) > 0:
+    pyplot.scatter(n_spots, n_indexed, marker=marker, alpha=alpha, c=blue, lw=lw)
+    xlim = pyplot.xlim()
+    ylim = pyplot.ylim()
+    pyplot.plot([0, max(n_spots)], [0, max(n_spots)], c=red)
+    pyplot.xlim(0, xlim[1])
+    pyplot.ylim(0, ylim[1])
+    pyplot.xlabel('# spots')
+    pyplot.ylabel('# indexed')
+    pyplot.savefig('n_spots_vs_n_indexed.png')
+    pyplot.clf()
 
-  pyplot.scatter(
+    pyplot.scatter(
     n_spots, fraction_indexed, marker=marker, alpha=alpha, c=blue, lw=lw)
-  pyplot.xlim(0, pyplot.xlim()[1])
-  pyplot.ylim(0, pyplot.ylim()[1])
-  pyplot.xlabel('# spots')
-  pyplot.ylabel('Fraction indexed')
-  pyplot.savefig('n_spots_vs_fraction_indexed.png')
-  pyplot.clf()
+    pyplot.xlim(0, pyplot.xlim()[1])
+    pyplot.ylim(0, pyplot.ylim()[1])
+    pyplot.xlabel('# spots')
+    pyplot.ylabel('Fraction indexed')
+    pyplot.savefig('n_spots_vs_fraction_indexed.png')
+    pyplot.clf()
 
-  pyplot.scatter(
-    n_indexed, fraction_indexed, marker=marker, alpha=alpha, c=blue, lw=lw)
-  pyplot.xlim(0, pyplot.xlim()[1])
-  pyplot.ylim(0, pyplot.ylim()[1])
-  pyplot.xlabel('# indexed')
-  pyplot.ylabel('Fraction indexed')
-  pyplot.savefig('n_indexed_vs_fraction_indexed.png')
-  pyplot.clf()
+    pyplot.scatter(
+      n_indexed, fraction_indexed, marker=marker, alpha=alpha, c=blue, lw=lw)
+    pyplot.xlim(0, pyplot.xlim()[1])
+    pyplot.ylim(0, pyplot.ylim()[1])
+    pyplot.xlabel('# indexed')
+    pyplot.ylabel('Fraction indexed')
+    pyplot.savefig('n_indexed_vs_fraction_indexed.png')
+    pyplot.clf()
 
-  pyplot.scatter(
-    n_spots, n_lattices, marker=marker, alpha=alpha, c=blue, lw=lw)
-  pyplot.xlim(0, pyplot.xlim()[1])
-  pyplot.ylim(0, pyplot.ylim()[1])
-  pyplot.xlabel('# spots')
-  pyplot.ylabel('# lattices')
-  pyplot.savefig('n_spots_vs_n_lattices.png')
-  pyplot.clf()
+    pyplot.scatter(
+      n_spots, n_lattices, marker=marker, alpha=alpha, c=blue, lw=lw)
+    pyplot.xlim(0, pyplot.xlim()[1])
+    pyplot.ylim(0, pyplot.ylim()[1])
+    pyplot.xlabel('# spots')
+    pyplot.ylabel('# lattices')
+    pyplot.savefig('n_spots_vs_n_lattices.png')
+    pyplot.clf()
 
   pyplot.scatter(
     estimated_d_min, d_min_distl_method_1, marker=marker, alpha=alpha, c=blue, lw=lw)
