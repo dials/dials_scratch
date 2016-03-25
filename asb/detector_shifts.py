63d7832dd1d3a7ac01d6276b5e97736c8800c535
Aaron Brewster <asbrewster@lbl.gov>
2016-03-25 11:44:22 -0700

    detector_shift.py: reformat as table, add X, Y and delta XY shifts.

--- asb/detector_shifts.py
+++ asb/detector_shifts.py
@@ -80,9 +80,19 @@ class Script(ParentScript):
     r_norm = col(reference_root.get_normal())
     s0 = col(flex.vec3_double([col(b.get_s0()) for b in experiments.beams()]).mean())
 
+    table_header = ["Hierarchy", "X Offsets", "X Offsets", "Y Offsets", "Y Offsets", "Delta XY", "Delta XY", "Z Offsets", "Z Offsets" ]
+    table_header2 = ["Level","","Sigma","","Sigma","","Sigma", "", "Sigma"]
+    table_header3 = ["","(microns)","(microns)","(microns)","(microns)","(microns)","(microns)", "(microns)", "(microns)"]
+    table_data = []
+    table_data.append(table_header)
+    table_data.append(table_header2)
+    table_data.append(table_header3)
+
     for level in xrange(4):
-      print "Z offsets at level", level
-      data = flex.double()
+      x_offsets = flex.double()
+      y_offsets = flex.double()
+      delta_xy = flex.double()
+      z_offsets = flex.double()
       weights = flex.double()
 
       for pg_id, (pg1, pg2) in enumerate(zip(iterate_detector_at_level(reference_root, 0, level),
@@ -92,18 +102,38 @@ class Script(ParentScript):
           weight += len(reflections.select(reflections['panel'] == id_from_name(detector, p.get_name())))
         weights.append(weight)
 
-        dists = []
+        x_dists = []
+        y_dists = []
+        z_dists = []
+        ori_xy = []
         for pg in [pg1,pg2]:
           ori = pg.get_local_origin()
-          dists.append(ori[2]*1000)
-        data.append(dists[1]-dists[0])
+          x_dists.append(ori[0]*1000)
+          y_dists.append(ori[1]*1000)
+          ori_xy.append(col((ori[0], ori[1])))
+          z_dists.append(ori[2]*1000)
+        x_offsets.append(x_dists[1]-x_dists[0])
+        y_offsets.append(y_dists[1]-y_dists[0])
+        delta_xy.append((ori_xy[1]-ori_xy[0]).length()*1000)
+        z_offsets.append(z_dists[1]-z_dists[0])
 
-      if len(data) > 1:
-        stats = flex.mean_and_variance(data, weights)
-        print r"Weighted mean %5.1f microns"%stats.mean()
-        print r"Weighted standard deviation %5.1f microns"%stats.gsl_stats_wsd()
+      row = ["%d"%level]
+      if len(z_offsets) == 0:
+        row.extend(["%6.1f"%0]*6)
+      elif len(z_offsets) == 1:
+        for data in [x_offsets, y_offsets, delta_xy, z_offsets]:
+          row.append("%6.1f"%data[0])
+          row.append("%6.1f"%0)
       else:
-        print r"Single value %5.1f microns"%data[0]
+        for data in [x_offsets, y_offsets, delta_xy, z_offsets]:
+          stats = flex.mean_and_variance(data, weights)
+          row.append("%6.1f"%stats.mean())
+          row.append("%6.1f"%stats.gsl_stats_wsd())
+      table_data.append(row)
+
+    from libtbx import table_utils
+    print "Detector shifts"
+    print table_utils.format(table_data,has_header=3,justify='center',delim=" ")
 
 if __name__ == '__main__':
   from dials.util import halraiser
