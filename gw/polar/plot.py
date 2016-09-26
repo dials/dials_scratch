3aaccc032604fe79873eef5363a7f262c949b2d6
graeme-winter <graeme.winter@gmail.com>
2016-09-26 08:35:51 +0100

    Looking at spherical harmonics

---
+++ gw/polar/plot.py
@@ -0,0 +1,47 @@
+import numpy
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as axes3d
+
+def sph_harm_surf(l, m):
+  from scitbx import math
+  from scitbx.array_family import flex
+  import math as pymath
+  lfg = math.log_factorial_generator(2 * l + 1)
+  nsssphe = math.nss_spherical_harmonics(l, 50000, lfg)
+
+  theta = numpy.linspace(0, 2 * numpy.pi, 80)
+  phi = numpy.linspace(0, numpy.pi, 40)
+
+  THETA, PHI = numpy.meshgrid(theta, phi)
+  R = numpy.cos(PHI**2)
+
+  sqrt2 = pymath.sqrt(2)
+
+  for it, t in enumerate(theta):
+    for ip, p in enumerate(phi):
+      Ylm = nsssphe.spherical_harmonic(l, abs(m), t, p)
+      if m < 0:
+        r = sqrt2 * ((-1) ** m) * Ylm.imag
+      elif m == 0:
+        assert(Ylm.imag == 0.0)
+        r = Ylm.real
+      else:
+        r = sqrt2 * ((-1) ** m) * Ylm.real
+      R[ip, it] = r
+
+  X = R * numpy.sin(PHI) * numpy.cos(THETA)
+  Y = R * numpy.sin(PHI) * numpy.sin(THETA)
+  Z = R * numpy.cos(PHI)
+
+  fig = plt.figure()
+  ax = fig.add_subplot(1,1,1, projection='3d')
+  plot = ax.plot_surface(
+      X, Y, Z, rstride=1, cstride=1, cmap=plt.get_cmap('jet'),
+      linewidth=0, antialiased=True, alpha=0.5)
+
+  plt.show()
+
+import sys
+
+l, m = map(int, sys.argv[1:3])
+sph_harm_surf(l, m)
