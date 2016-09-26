2d36cc54c80531ab0c6befc82d9f576aadf37f7a
graeme-winter <graeme.winter@gmail.com>
2016-09-26 15:07:49 +0100

    Better script; run refine.sh [0-5]

--- metro_paper_0/i03_split_6/refine.sh
+++ metro_paper_0/i03_split_6/refine.sh
@@ -1,10 +1,10 @@
 . ~/svn/setup.sh
 
-run=$1
-
 export P6M_60_PANEL=1
 
-dials.index datablock.json strong.pickle space_group=P4 \
+mkdir ${1}
+cd ${1}
+dials.index ../datablock-${1}.json ../strong-${1}.pickle space_group=P4 \
 reflections_per_degree=None
 dials.refine indexed.pickle experiments.json hierarchy_level=1 \
 reflections_per_degree=None ../refine.phil
