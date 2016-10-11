23e9aee4ba3db6898d1b54759eb8f5d8e955e254
rjgildea <richard.gildea@diamond.ac.uk>
2016-10-11 20:06:51 +0100

    more descriptive output

--- rjg/time_as_double.py
+++ rjg/time_as_double.py
@@ -5,12 +5,9 @@ a = flex.int(flex.grid(2527, 2463))
 t0 = time.time()
 for i in range(n):
   b = a.as_double()
-  #c = b.iround()
-  #c = flex.double(flex.grid(2527, 2463))
-  #c = flex.random_double(a.size())
 t1 = time.time()
 t = t1-t0
-print "%.2fs (%.3fs/call)" %(t, t/n)
+print "flex.int.as_double(): %.2fs (%.3fs/call)" %(t, t/n)
 
 import numpy as np
 a = np.random.randint(100, size=a.all())
@@ -19,4 +16,4 @@ for i in range(n):
   b = a.astype(np.float64)
 t1 = time.time()
 t = t1-t0
-print "%.2fs (%.3fs/call)" %(t, t/n)
+print "numpy.astype(np.float64): %.2fs (%.3fs/call)" %(t, t/n)
