9d47426a3321a09669f4312ac7c40ac3175f022f
rjgildea <richard.gildea@diamond.ac.uk>
2016-12-14 10:58:34 +0000

    some more plots based on nearest neighbour analysis

--- rjg/nearest_neighbour_analysis.py
+++ rjg/nearest_neighbour_analysis.py
@@ -149,11 +149,99 @@ def run(args):
   max_cell = result.max_cell
   print "Found max_cell: %.1f Angstrom" %max_cell
   result.plot_histogram()
+  plot_d_spacings(reflections)
+  plot_direct_space_distances(result.direct, result.d_spacings)
   #logger.info("Found max_cell: %.1f Angstrom" %(max_cell))
 
-
-
   return
+
+
+def plot_d_spacings(reflections):
+  from cctbx import miller, sgtbx, uctbx
+  from matplotlib import pyplot as plt
+
+  d_spacings = 1/reflections['rlp'].norms()
+  d_star_sq = uctbx.d_as_d_star_sq(d_spacings)
+
+  ice_uc = uctbx.unit_cell((4.498, 4.498, 7.338,90,90,120))
+  ice_sg = sgtbx.space_group_info(number=194).group()
+  ice_generator = miller.index_generator(ice_uc, ice_sg.type(), False, flex.min(d_spacings))
+  ice_indices = ice_generator.to_array()
+  ice_d_spacings = flex.sorted(ice_uc.d(ice_indices))
+  ice_d_star_sq = uctbx.d_as_d_star_sq(ice_d_spacings)
+
+  cubic_ice_uc = uctbx.unit_cell((6.358, 6.358, 6.358,90,90,90))
+  cubic_ice_sg = sgtbx.space_group_info(number=227).group()
+  cubic_ice_generator = miller.index_generator(cubic_ice_uc, cubic_ice_sg.type(), False, flex.min(d_spacings))
+  cubic_ice_indices = cubic_ice_generator.to_array()
+  cubic_ice_d_spacings = flex.sorted(cubic_ice_uc.d(cubic_ice_indices))
+  cubic_ice_d_star_sq = uctbx.d_as_d_star_sq(cubic_ice_d_spacings)
+
+  perm = flex.sort_permutation(d_star_sq)
+  plt.plot(d_star_sq.select(perm), flex.int_range(perm.size()), zorder=10)
+  for dss in ice_d_star_sq:
+    plt.plot([dss, dss], plt.ylim(), c='r', zorder=0)
+  for dss in cubic_ice_d_star_sq:
+    plt.plot([dss, dss], plt.ylim(), c='g', zorder=1)
+  ax = plt.gca()
+  xticks = ax.get_xticks()
+  xticks_d = [uctbx.d_star_sq_as_d(x) for x in xticks]
+  ax.set_xticklabels(['%.2f' %x for x in xticks_d])
+  plt.xlabel('d spacing (A^-1)')
+  plt.ylabel('Cumulative frequency')
+  plt.savefig('d_spacings.png')
+  plt.clf()
+
+  intensities = reflections['intensity.sum.value']
+  plt.scatter(uctbx.d_as_d_star_sq(d_spacings), intensities, marker='.',
+              c='black', s=1)
+  ax = plt.gca()
+  xticks = ax.get_xticks()
+  xticks_d = [uctbx.d_star_sq_as_d(x) for x in xticks]
+  ax.set_xticklabels(['%.2f' %x for x in xticks_d])
+  plt.xlabel('d spacing (A^-1)')
+  plt.ylabel('Intensity')
+  plt.savefig('d_vs_intensity.png')
+  plt.clf()
+
+  hist = flex.histogram(d_star_sq, n_slots=200)
+  plt.bar(hist.slot_centers(), hist.slots(), align="center",
+          width=hist.slot_width(), zorder=10, color='black', edgecolor=None)
+  for dss in ice_d_star_sq:
+    plt.plot([dss, dss], plt.ylim(), c='r', zorder=0)
+  for dss in cubic_ice_d_star_sq:
+    plt.plot([dss, dss], plt.ylim(), c='g', zorder=0)
+  ax = plt.gca()
+  xticks = ax.get_xticks()
+  xticks_d = [uctbx.d_star_sq_as_d(x) for x in xticks]
+  ax.set_xticklabels(['%.2f' %x for x in xticks_d])
+  plt.xlabel('d spacing (A^-1)')
+  plt.ylabel('Frequency')
+  plt.savefig('d_spacings_hist.png')
+  plt.clf()
+
+def plot_direct_space_distances(direct, d_spacings):
+  from cctbx import uctbx
+  from matplotlib import pyplot as plt
+  plt.plot(direct, flex.double_range(direct.size())/direct.size())
+  ax = plt.gca()
+  ax.set_xscale('log')
+  plt.xlabel('Direct space distance (A)')
+  plt.ylabel('Percentile')
+  plt.savefig('ordered.png')
+  plt.clf()
+
+  fig = plt.figure(figsize=(12,8))
+  plt.scatter(uctbx.d_as_d_star_sq(d_spacings), direct, marker='.', c='black', s=1)
+  ax = plt.gca()
+  ax.set_yscale('log')
+  xticks = ax.get_xticks()
+  xticks_d = [uctbx.d_star_sq_as_d(x) for x in xticks]
+  ax.set_xticklabels(['%.2f' %x for x in xticks_d])
+  plt.ylabel('Direct space distance (A)')
+  plt.xlabel('d spacing(A^-1)')
+  plt.savefig('d_vs_direct.png')
+  plt.clf()
 
 
 if __name__ == '__main__':
