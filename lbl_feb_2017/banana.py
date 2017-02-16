a3733134b92e429306be1b45d9412e52f7f881c7
graeme-winter <graeme.winter@gmail.com>
2017-02-16 03:54:23 -0800

    Other plotting options; FIXME make this more useful

--- lbl_feb_2017/banana.py
+++ lbl_feb_2017/banana.py
@@ -4,16 +4,23 @@ import sys
 
 reflection_file = sys.argv[1]
 data = pickle.load(open(reflection_file, 'rb'))
-print '%d reflections' % data.size()
 
 i = data['intensity.sum.value']
 v = data['intensity.sum.variance']
 s = flex.sqrt(v)
 i_s = i / s
+dq = data['dq']
 
-h = flex.histogram(i_s, -5, 20, n_slots=50)
-c = h.slot_centers()
-d = h.slots()
+def histogram():
+  h = flex.histogram(i_s, -5, 20, n_slots=50)
+  c = h.slot_centers()
+  d = h.slots()
 
-for _c, _d in zip(c, d):
-  print _c, _d
+  for _c, _d in zip(c, d):
+    print _c, _d
+
+def scatter():
+  for j in range(i_s.size()):
+    print i_s[j], dq[j]
+
+scatter()
