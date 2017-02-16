635412b0dda44a9bf9535d7e4956bf05f8718c32
James Parkhurst <james.parkhurst@diamond.ac.uk>
2017-02-16 22:09:05 +0000

    Find spots from flex array

---
+++ jmp/find_spots_from_flex.py
@@ -0,0 +1,45 @@
+
+from dials.array_family import flex
+
+def find_spots(image, mask, min_spot_size=1, max_spot_size=1000):
+  from dials.algorithms.spot_finding.threshold import XDSThresholdStrategy
+  from dials.model.data import PixelList
+  from dials.model.data import PixelListLabeller
+  threshold_image = XDSThresholdStrategy()
+
+  threshold_mask = threshold_image(image, mask)
+  plist = PixelList(0, image, threshold_mask)
+    
+  pixel_labeller = PixelListLabeller()
+
+  pixel_labeller.add(plist)
+
+  creator = flex.PixelListShoeboxCreator(
+      pixel_labeller,
+      0,                   # panel
+      0,                   # zrange
+      True,                # twod
+      min_spot_size,  # min_pixels
+      max_spot_size,  # max_pixels
+      False)
+  shoeboxes = creator.result()
+
+  centroid = shoeboxes.centroid_valid()
+  intensity = shoeboxes.summed_intensity()
+  observed = flex.observation(shoeboxes.panels(), centroid, intensity)
+
+  return flex.reflection_table(observed, shoeboxes)
+
+
+
+if __name__ == '__main__':
+  import sys
+  from dxtbx.datablock import DataBlockFactory
+  db = DataBlockFactory.from_json_file(sys.argv[1])
+  iset = db[0].extract_imagesets()[0]
+  image = iset.get_raw_data(0)[0]
+  mask = iset.get_mask(0)[0]
+
+  reflections = find_spots(image, mask)
+
+  print len(reflections)
