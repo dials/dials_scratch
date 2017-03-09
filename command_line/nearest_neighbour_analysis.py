3e0679635e5764681a4c9aa088afdcbcd1448f9c
rjgildea <richard.gildea@diamond.ac.uk>
2017-03-09 15:09:23 +0000

    add dev.dials.nearest_neighbour_analysis utility command

---
+++ command_line/nearest_neighbour_analysis.py
@@ -0,0 +1,7 @@
+# LIBTBX_SET_DISPATCHER_NAME dev.dials.nearest_neighbour_analysis
+from dials_scratch.rjg.nearest_neighbour_analysis import run
+
+if __name__ == '__main__':
+  import sys
+  run(sys.argv[1:])
+
