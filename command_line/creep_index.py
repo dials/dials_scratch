5dbf9929c617011793dd7bcf7b83c39cfd1bea87
dgwaterman <dgwaterman@gmail.com>
2017-03-13 14:45:56 +0000

    keep track of concatenated lists of indexed reflections, output improvement

--- command_line/creep_index.py
+++ command_line/creep_index.py
@@ -95,29 +95,30 @@ class Script(object):
     print "{0} strong spots read from {1}".format(self._num_strong, self._strong_path)
 
     self._images_per_block = params.images_per_block
+
+    self._all_indexed = None
     return
 
-  def _index_current_block(self, start, stop):
+  def _index_current_block(self, job_id, start, stop):
 
     fmt_dic = {'experiments':self._current_exp_path,
                'indexed':self._strong_path,
                'start':start,
-               'stop':stop}
+               'stop':stop,
+               'job_id':job_id}
 
     cmd = ('dials.index {experiments} {indexed} '
            'scan_range={start},{stop} '
-           'output.experiments=experiments_{start}_{stop}.json '
-           'output.reflections=indexed_{start}_{stop}.pickle '
+           'output.experiments=latest_experiments.json '
+           'output.reflections=indexed_{job_id:03d}.pickle '
            'output.log=None output.debug_log=None').format(**fmt_dic)
     result = easy_run.fully_buffered(command=cmd)
 
     # check if indexing worked
-    new_exp_path = 'experiments_{start}_{stop}.json'.format(**fmt_dic)
-    indexed_path = 'indexed_{start}_{stop}.pickle'.format(**fmt_dic)
+    new_exp_path = 'latest_experiments.json'
+    indexed_path = 'indexed_{job_id:03d}.pickle'.format(**fmt_dic)
     tst = [os.path.exists(e) for e in (new_exp_path, indexed_path)]
     if tst.count(True) != 2:
-      # print the errors
-      #for l in result.stderr_lines: print l
       return (None, None)
     return new_exp_path, indexed_path
 
@@ -129,10 +130,13 @@ class Script(object):
     stop = first
 
     # set up table
-    header = ["Experiments", "#Idx", "Cell", "Beam centre\n(fast,slow)",
+    header = ["Job", "Scan\nrange", "#Idx", "Cell", "Beam centre\n(fast,slow)",
       "Distance\n(mm)", "RMSD_X\n(px)", "RMSD_Y\n(px)", "RMSD_Z\n(px)"]
     rows = []
     num_indexed = []
+    filelist_lines = []
+    job_id = 1
+
     while True:
 
       # finish if we already processed all blocks
@@ -144,7 +148,7 @@ class Script(object):
         stop = start + nblocks * self._images_per_block
         # if within one block size from the end, include all images up to the end
         if stop >= last - self._images_per_block: stop = last
-        new_exp_path, indexed_path = self._index_current_block(start, stop)
+        new_exp_path, indexed_path = self._index_current_block(job_id, start, stop)
         # exit if successful indexing
         if [new_exp_path, indexed_path].count(None) == 0: break
         # exit if no more data to include
@@ -156,7 +160,7 @@ class Script(object):
 
       # it worked, so update the pointer to the current model
       self._current_exp_path = new_exp_path
-      print "Indexed", self._current_exp_path
+      print "Job {0} completed: scan range ({1},{2})".format(job_id, start, stop)
 
       # load the indexed results
       el = ExperimentListFactory.from_json_file(self._current_exp_path,
@@ -165,6 +169,11 @@ class Script(object):
       exp = el[0]
 
       rt = flex.reflection_table.from_pickle(indexed_path)
+      if self._all_indexed is None:
+        self._all_indexed = rt
+      else:
+        self._all_indexed.extend(rt)
+
       indexed = rt.select(rt.get_flags(rt.flags.indexed))
       num_indexed.append(len(indexed))
       panel_id, (x,y) = beam_centre_mm(exp.detector, exp.beam)
@@ -172,7 +181,8 @@ class Script(object):
       indexed_rmsds = self._rmsds(indexed)
       cell = exp.crystal.get_unit_cell().parameters()
       pnl_dists = [p.get_distance() for p in exp.detector]
-      rows.append([self._current_exp_path,
+      rows.append(["{0}".format(job_id),
+                   "({0},{1})".format(start, stop),
                    "%i" % num_indexed[-1],
                    "%.2f %.2f %.2f %.2f %.2f %.2f" % cell,
                    "%.2f %.2f" % bc,
@@ -186,10 +196,13 @@ class Script(object):
         exp.scan.swap(exp.scan[start:stop])
 
       dump = ExperimentListDumper(el)
-      dump.as_json("sliced_" + self._current_exp_path)
+      dump.as_json("experiments_{0:03d}.json".format(job_id))
 
-      # update start image for the next block
+      filelist_lines.append("block{0:03d} experiments_{0:03d}.json ".format(job_id) + indexed_path)
+
+      # set up for the next block
       start = stop
+      job_id += 1
 
     # print results
     st = simple_table(rows, header)
@@ -197,6 +210,16 @@ class Script(object):
     total_num_indexed = sum(num_indexed)
     print "{0} indexed reflections out of {1} strong spots ({2:.1f}%)".format(
       total_num_indexed, self._num_strong, 100.*total_num_indexed/self._num_strong)
+
+    # write out the list of files
+    print "writing out list of experiments and indexed reflections to filelist.txt"
+    with open("filelist.txt", "w") as f:
+      f.write("\n".join(filelist_lines))
+      f.write("\n")
+
+    # write out the full reflection table of indexed reflections
+    print "writing out all indexed reflections to all_indexed.pickle"
+    self._all_indexed.as_pickle('all_indexed.pickle')
 
   def _rmsds(self, reflections):
     """calculate unweighted RMSDs for the specified reflections"""
