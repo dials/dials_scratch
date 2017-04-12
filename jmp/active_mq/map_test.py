7f55a86f8f9944a544af1744f53d7f1654ec278d
James Parkhurst <james.parkhurst@diamond.ac.uk>
2017-04-12 15:32:59 +0100

    Added some active mq testsing stuff

---
+++ jmp/active_mq/map_test.py
@@ -0,0 +1,130 @@
+from __future__ import division
+import workflows
+from workflows.transport.stomp_transport import StompTransport
+from threading import Thread, Event
+
+
+class Sender(object):
+  
+  def __init__(self, transport):
+    self.transport = transport
+    
+  def send(self, message):
+    self.transport.send(
+      'outbound',
+      message
+    )
+
+class Receiver(object):
+ 
+  def __init__(self, transport):
+    self.transport = transport
+    
+    self.message_list = []
+    
+    self.transport.subscribe("inbound", self.read)
+
+  def read(self, header, message):
+    self.message_list.append(message)    
+
+
+class HeartBeat(Thread):
+
+  def __init__(self, transport):
+    super(HeartBeat, self).__init__()
+    self.transport = transport
+    self.stop_event = Event()
+
+  def run(self):
+    while not self.stop_event.wait(0.5):
+      self.transport.broadcast("heartbeat", "")
+
+  def stop(self):
+    self.stop_event.set()
+
+
+class Master(object):
+
+  def __init__(self):
+    
+    self.transport = StompTransport()
+    self.transport.connect()
+
+    self.heartbeat = HeartBeat(self.transport) 
+    self.heartbeat.start()
+
+    self.sender = Sender(self.transport)
+    self.receiver = Receiver(self.transport)
+    self.num_messages = 0
+
+  def send(self, message):
+    self.num_messages += 1
+    self.sender.send(message)
+
+  def receive(self):
+
+    try:
+      from time import sleep
+      while len(self.receiver.message_list) != self.num_messages:
+        print len(self.receiver.message_list)
+        sleep(1)
+      self.heartbeat.stop()
+    except KeyboardInterrupt:
+      self.heartbeat.stop()
+      raise
+
+    return self.receiver.message_list
+
+
+def cluster_map(
+    func,
+    iterable,
+    callback=None,
+    nslots=1,
+    njobs=1,
+    job_category="low"):
+  from multiprocessing import Process
+
+  processes = [Process(target=Worker()) for i in range(len(njobs))]
+  for p in processes:
+    p.start()
+
+
+  
+
+  for p in processes:
+    p.join()
+
+
+class Data(object):
+
+  def __init__(self, a, b, c):
+    self.a = a
+    self.b = b
+    self.c = c
+
+
+if __name__ == '__main__':
+  import cPickle as pickle
+
+  configuration = '/dls_sw/apps/zocalo/secrets/credentials-james.cfg'
+
+  StompTransport.load_configuration_file(configuration)
+
+  data = Data(1, 2, 3)
+
+  message = { 
+    'hello': 'world',
+    'data' : pickle.dumps(data),
+  }
+
+  master = Master()
+  master.send(message)
+  master.send(message)
+  master.send(message)
+  master.send(message)
+  print master.receive() 
+
+
+  print "\nSubmitted."
+
---
+++ jmp/active_mq/worker.py
@@ -0,0 +1,71 @@
+from __future__ import absolute_import, division
+from dlstbx.util.colorstreamhandler import ColorStreamHandler
+import json
+import logging
+from optparse import OptionParser, SUPPRESS_HELP
+import sys
+import time
+from workflows.transport.stomp_transport import StompTransport
+
+
+
+class Worker(object):
+
+  def __init__(self, wait_time=5):
+
+    self.wait_time = wait_time
+    self.heartbeat_timestamp = None
+
+    self.transport = StompTransport()
+    self.transport.connect()
+    self.transport.subscribe_broadcast("heartbeat", self.read_heartbeat)
+    self.transport.subscribe("outbound", self.read)
+ 
+    self.wait()
+
+  def timeout(self):
+    from time import time
+    if self.heartbeat_timestamp is None:
+      self.heartbeat_timestamp = time()
+    else:
+      if time() - self.heartbeat_timestamp > self.wait_time:
+        return True
+    return False
+
+  def wait(self):
+    from time import time, sleep
+    try:
+      while not self.timeout():
+        sleep(1)
+    except KeyboardInterrupt:
+      pass
+
+  def send(self, message):
+    print "Sending"
+    self.transport.send(
+      'inbound',
+      message
+    )
+
+  def read_heartbeat(self, header, message):
+    from time import time
+    self.heartbeat_timestamp = time()
+
+  def read(self, header, message):
+      message = {
+        "result" : "Hey Hey!"
+      }
+      self.send(message)
+
+
+
+if __name__ == '__main__':
+  configuration = '/dls_sw/apps/zocalo/secrets/credentials-james.cfg'
+  
+  from workflows.transport.stomp_transport import StompTransport
+  
+  StompTransport.load_configuration_file(configuration)
+
+  worker = Worker()
+
+
