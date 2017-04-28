4162911fe53dbea88456c038309ee082ef031bd4
James Parkhurst <james.parkhurst@diamond.ac.uk>
2017-04-28 09:08:05 +0100

    Added some test scripts for bayesian inference

---
+++ jmp/bayesian_stuff/test_estimated_background.py
@@ -0,0 +1,173 @@
+from __future__ import division
+
+from numpy.random import seed
+seed(100)
+
+
+
+def gen_shapes(N):
+  from math import exp
+  mid = N / 2
+  sigma = (N / 2)/5
+  bg = [1] * N
+  fg = [0] * N
+  mk = [False] * N
+  for i in range(N):
+    fg[i] = exp(-(i-mid)**2 / (2*sigma**2))
+    mk[i] = abs(i - mid) < 3*sigma
+  bg = [b / sum(bg) for b in bg]
+  fg = [f / sum(fg) for f in fg]
+  return bg, fg, mk
+
+def simulate(B, S, bg, fg):
+  from numpy.random import poisson
+  data = [poisson(B * b + S * s) for b, s in zip(bg, fg)]
+  return data
+
+def estimate(data, background, mk):
+  from math import factorial
+  N = mk.count(True)
+  M = mk.count(False)
+  Cb = 0
+  Cf = 0
+  for i in range(len(data)):
+    if mk[i]:
+      Cf += data[i]
+    else:
+      Cb += data[i]
+  s1 = 0
+  for j in range(Cf+1):
+    s1 += (1 + M/N)**j * factorial(Cf + Cb - j) / factorial(Cf - j)
+  s2 = 0
+  for i in range(Cf+1):
+    Ci = ((1 + M/N)**i * factorial(Cf + Cb - i) / factorial(Cf - i))# / s1
+    s2 += Ci * (i+1)
+  return s2 / (N * s1)
+
+
+# def plot_weights(data, background, mk):
+#   from math import exp, factorial
+#   N = mk.count(True)
+#   M = mk.count(False)
+#   Cb = 0
+#   Cf = 0
+
+#   X = []
+#   Y = []
+
+#   for i in range(len(data)):
+#     if mk[i]:
+#       Cf += data[i]
+#     else:
+#       Cb += data[i]
+#   for j in range(Cf+1):
+#     a = (1 + M/N)**(j-Cf) 
+#     b = factorial(Cf + Cb - j) / factorial(Cf - j)
+#     jj = a
+#     jj = a*b
+#     X.append(j)
+#     Y.append(jj)
+
+#   from matplotlib import pylab
+#   pylab.plot(X,Y)
+#   pylab.show()
+
+def Ps(data, background, mk, s):
+  from math import exp, factorial
+  N = mk.count(True)
+  M = mk.count(False)
+  Cb = 0
+  Cf = 0
+
+  for i in range(len(data)):
+    if mk[i]:
+      Cf += data[i]
+    else:
+      Cb += data[i]
+  s1 = 0
+  for j in range(Cf+1):
+    jj = (1 + M/N)**j * factorial(Cf + Cb - j) / factorial(Cf - j)
+    s1+= jj
+
+  s2 = 0
+  for i in range(Cf+1):
+    Ci = ((1 + M/N)**i * factorial(Cf + Cb - i) / factorial(Cf - i)) / s1
+    s2 += Ci * N*(s*N)**i * exp(-s*N) / factorial(i)
+  return s2
+
+# def Ps2(data, background, mk, s):
+#   from math import exp, factorial
+#   N = mk.count(True)
+#   M = mk.count(False)
+#   Cb = 0
+#   Cf = 0
+#   for i in range(len(data)):
+#     if mk[i]:
+#       Cf += data[i]
+#     else:
+#       Cb += data[i]
+#   s1 = 0
+#   for k in range(Cf+1):
+#     a = factorial(Cf) / (factorial(k)*factorial(Cf-k))
+#     b = s**k * exp(-s*N)
+#     c = (N+M)**(-(Cf+Cb-k)-1) * factorial(Cf+Cb-k)
+#     s1 += a * b * c
+#   s2 = 0
+#   for i in range(Cf+1):
+#     a = factorial(Cf) / (factorial(k)*factorial(Cf-k))
+#     b = N**(-k-1) * factorial(k)
+#     c = (N+M)**(-(Cf+Cb-k)-1) * factorial(Cf+Cb-k)
+#     s2 += a * b * c
+#   return (s1 / s2)
+
+
+bg, fg, mk = gen_shapes(20)
+
+B = 100.0
+S = 1.0
+
+data = simulate(B, S, bg, fg)
+background = [B * bg[i] for i in range(len(data))]
+
+
+# plot_weights(data, background, mk)
+# exit(0)
+
+Nb = mk.count(False)
+Nf = mk.count(True)
+Cb = 0
+Cf = 0
+for i in range(len(data)):
+  if mk[i]:
+    Cf += data[i]
+  else:
+    Cb += data[i]
+
+print Cb, Cf, Nb, Nf
+
+X = []
+Y1 = []
+Y2 = []
+
+
+
+
+
+X = []
+Y1 = []
+
+print Cf, Cb, (Cf - Cb) / Nb
+
+from math import log
+for i in range(100):
+  s = -1.6 + i * 0.1
+  p1 = Ps(data, background, mk, s)
+  X.append(s)
+  Y1.append(p1)
+
+
+print estimate(data, background, mk) * mk.count(True)
+
+from matplotlib import pylab
+pylab.plot(X, Y1)
+pylab.show()
---
+++ jmp/bayesian_stuff/test_known_background.py
@@ -0,0 +1,92 @@
+from __future__ import division
+
+def simple_summation(data, background):
+  '''
+  Perform a simple summation with background subtraction
+
+  '''
+  signal = 0
+  for C, B in zip(data, background):
+    signal += C - B
+  return signal
+
+
+def estimate_signal(data, background, estimator='mean'):
+  '''
+  :param data: The array of pixel data
+  :param background: The array of known background values
+  :param estimator: Mean or Maximum of Posterior distribution (mean, max)
+
+  :returns: A bayesian estimate of the signal
+
+  '''
+  if estimator == 'mean':
+
+    C = sum(data)
+    B = sum(background)
+
+    from scipy.special import gammaincc
+    K = gammaincc(C+1, B)
+    return K*((C+1)*gammaincc(C+2, B) - B*gammaincc(C+1, B))
+
+  elif estimator == 'max':
+    
+    C = sum(data)
+    B = sum(background)
+    
+    return max(0, C - B)
+
+  else:
+    raise RuntimeError('BANG!')
+
+
+
+
+def gen_shapes(N):
+  from math import exp
+  mid = N / 2
+  sigma = (N / 2)/5
+  bg = [1] * N
+  fg = [0] * N
+  mk = [False] * N
+  for i in range(N):
+    fg[i] = exp(-(i-mid)**2 / (2*sigma**2))
+    mk[i] = abs(i - mid) < 3*sigma
+  bg = [b / sum(bg) for b in bg]
+  fg = [f / sum(fg) for f in fg]
+  return bg, fg, mk
+
+def simulate(B, S, bg, fg):
+  from numpy.random import poisson
+  data = [poisson(B * b + S * s) for b, s in zip(bg, fg)]
+  return data
+
+bg, fg, mk = gen_shapes(20)
+
+B = 100.0
+S = 1.0
+
+Y1 = []
+Y2 = []
+Y3 = []
+
+for t in range(2000):
+
+  data = simulate(B, S, bg, fg)
+  background = [B * bg[i] for i in range(len(data))]
+  
+  S_sum = simple_summation(data, background)
+  S_mean = estimate_signal(data, background, estimator='mean')
+  S_max = estimate_signal(data, background, estimator='max')
+
+  print S_sum, S_mean, S_max
+
+  Y1.append(S_sum)
+  Y2.append(S_mean)
+  Y3.append(S_max)
+
+from matplotlib import pylab
+pylab.hist(Y1, bins=20)
+pylab.hist(Y2, bins=20)
+#pylab.hist(Y3, bins=20)
+pylab.show()
