e6bce1ae39b5f8726879b9b96effb052e7c59af9
Aaron Brewster <asbrewster@lbl.gov>
2017-05-22 13:26:50 -0700

    Put everything in a run method

--- asb/random_split.py
+++ asb/random_split.py
@@ -19,40 +19,44 @@ phil_scope = parse("""
     .help = If not None, use the random split selection from a previous run
 """)
 
-user_phil = []
-for arg in sys.argv[1:]:
-  if os.path.isfile(arg):
-    filename = arg
+def run(args):
+  user_phil = []
+  for arg in args:
+    if os.path.isfile(arg):
+      filename = arg
+    else:
+      try:
+        user_phil.append(parse(arg))
+      except Exception, e:
+        raise Sorry("Unrecognized argument: %s"%arg)
+
+  params = phil_scope.fetch(sources = user_phil).extract()
+
+  name = os.path.basename(filename)
+  base, ext = os.path.splitext(name)
+  filea = base + "_a" + ext
+  fileb = base + "_b" + ext
+
+  data = easy_pickle.load(filename)
+
+  if params.use_selection is None:
+    sel = flex.random_permutation(len(data))
   else:
-    try:
-      user_phil.append(parse(arg))
-    except Exception, e:
-      raise Sorry("Unrecognized argument: %s"%arg)
+    sel = easy_pickle.load(params.use_selection)
+    assert len(sel) == len(data), "Length of selection doesn't match length of input"
 
-params = phil_scope.fetch(sources = user_phil).extract()
+  data_a = data.select(sel[:len(data)//2])
+  data_b = data.select(sel[len(data)//2:])
+  data_a = data_a.select(flex.sort_permutation(data_a['id']))
+  data_b = data_b.select(flex.sort_permutation(data_b['id']))
 
-name = os.path.basename(filename)
-base, ext = os.path.splitext(name)
-filea = base + "_a" + ext
-fileb = base + "_b" + ext
+  assert len(data_a) + len(data_b) == len(data)
 
-data = easy_pickle.load(filename)
+  easy_pickle.dump(filea, data_a)
+  easy_pickle.dump(fileb, data_b)
 
-if params.use_selection is None:
-  sel = flex.random_permutation(len(data))
-else:
-  sel = easy_pickle.load(params.use_selection)
-  assert len(sel) == len(data), "Length of selection doesn't match length of input"
+  if params.output_selection is not None:
+    easy_pickle.dump(params.output_selection, sel)
 
-data_a = data.select(sel[:len(data)//2])
-data_b = data.select(sel[len(data)//2:])
-data_a = data_a.select(flex.sort_permutation(data_a['id']))
-data_b = data_b.select(flex.sort_permutation(data_b['id']))
-
-assert len(data_a) + len(data_b) == len(data)
-
-easy_pickle.dump(filea, data_a)
-easy_pickle.dump(fileb, data_b)
-
-if params.output_selection is not None:
-  easy_pickle.dump(params.output_selection, sel)
+if __name__ == "__main__":
+  run(sys.argv[1:])
