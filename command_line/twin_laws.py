fdae805bb833272a75d156e193c1f503aba3a733
graeme-winter <graeme.winter@gmail.com>
2017-06-06 06:58:05 +0100

    Make general for reflection files; clean up code

--- command_line/twin_laws.py
+++ command_line/twin_laws.py
@@ -1,15 +1,19 @@
 from iotbx import mtz
 import sys
 
-m = mtz.object(sys.argv[1])
-mad = m.as_miller_arrays_dict()
-k = [k for k in mad.keys() if k[2] == 'I'][0]
-i = mad[k]
+from iotbx.file_reader import any_file
+f = any_file(sys.argv[1], force_type='hkl', raise_sorry_if_errors=True)
+
+# find intensity array - if XDS HKL file need to check label
+i = [ma for ma in f.file_content.as_miller_arrays() if
+      (ma.is_xray_intensity_array() or 'iobs' in ma.info().label_string())]
+
+if not i:
+  raise RuntimeError, 'no intensities found'
 
 from mmtbx.scaling.twin_analyses import twin_laws
-TL = twin_laws(miller_array=i)
+TL = twin_laws(miller_array=i[0])
 
 print '%d possible operators' % len(TL.operators)
 for o in TL.operators:
-    print o.delta_le_page
-    print o.operator
+    print 'Le-page delta: %.3f operator: %s' % (o.delta_le_page, o.operator)
