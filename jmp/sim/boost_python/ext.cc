3fa7506abe6ade5cdbaa462ef86526413bd981d7
James Parkhurst <james.parkhurst@diamond.ac.uk>
2017-06-20 16:41:21 +0100

    Added some simulation stuff

---
+++ jmp/sim/boost_python/ext.cc
@@ -0,0 +1,103 @@
+#include <random>
+#include <boost/python.hpp>
+#include <boost/python/def.hpp>
+#include <dials/array_family/scitbx_shared_and_versa.h>
+#include <dials/algorithms/profile_model/gaussian_rs/coordinate_system.h>
+#include <dxtbx/model/detector.h>
+#include <dxtbx/model/scan.h>
+#include <dials/error.h>
+
+using namespace boost::python;
+
+namespace dials { namespace algorithms { namespace boost_python {
+  
+  using dxtbx::model::Detector;
+  using dxtbx::model::Panel;
+  using dxtbx::model::Scan;
+  using dials::algorithms::profile_model::gaussian_rs::CoordinateSystem;
+  using scitbx::af::int6;
+  using scitbx::vec2;
+  using scitbx::vec3;
+
+  af::versa< double, af::c_grid<3> > compute_profile_internal(
+      af::const_ref< double, af::c_grid<3> > grid,
+      int6 bbox,
+      std::size_t zs,
+      std::size_t ys,
+      std::size_t xs,
+      std::size_t N,
+      double delta_d,
+      double delta_m,
+      const Detector &detector,
+      const Scan &scan,
+      const CoordinateSystem &cs) {
+
+    std::default_random_engine generator;
+    std::uniform_real_distribution<double> uniform(0.0,1.0);
+    
+    af::versa< double, af::c_grid<3> > profile(af::c_grid<3>(zs, ys, xs)); 
+  
+    double xoff = -delta_d;
+    double yoff = -delta_d;
+    double zoff = -delta_m;
+
+    double xstep = (2*delta_d) / grid.accessor()[2];
+    double ystep = (2*delta_d) / grid.accessor()[1];
+    double zstep = (2*delta_m) / grid.accessor()[0];
+
+    for (std::size_t k = 0; k < grid.accessor()[0]; ++k) {
+      for (std::size_t j = 0; j < grid.accessor()[1]; ++j) {
+        for (std::size_t i = 0; i < grid.accessor()[2]; ++i) {
+          double counts = grid(k,j,i);          
+          if (counts > 0) {
+            double fraction = counts / double(N);
+            for (std::size_t l = 0; l < N; ++l) {
+
+              double gz = uniform(generator) + k;
+              double gy = uniform(generator) + j;
+              double gx = uniform(generator) + i;
+              
+              double e1 = xoff + gx * xstep;
+              double e2 = yoff + gy * ystep;
+              double e3 = zoff + gz * zstep;
+              
+              vec3<double> s1p = cs.to_beam_vector(vec2<double>(e1, e2));
+              vec2<double> pxy = detector[0].get_ray_intersection_px(s1p);
+              double px = pxy[0] - bbox[0];
+              double py = pxy[1] - bbox[2];
+
+              double phip = cs.to_rotation_angle_fast(e3);
+              double pz = scan.get_array_index_from_angle(phip) - bbox[4];
+
+              int kk = (int)std::floor(pz);
+              int jj = (int)std::floor(py);
+              int ii = (int)std::floor(px);
+
+              /* std::cout << zs << ", " << cs.zeta() << ", " << cs.phi() << ", " << phip << ", " << kk << std::endl; */
+
+              /* std::cout << zs << ", " << ys << "," << xs << "->" << px << ", " << py << " " << pz << std::endl; */
+
+              if (kk >= 0 && kk < zs &&
+                  jj >= 0 && jj < ys &&
+                  ii >= 0 && ii < xs) {
+                profile(kk, jj, ii) += fraction;
+              }
+            }
+          }
+        }
+      }
+    }
+
+
+
+
+  
+    return profile;
+  }
+
+  BOOST_PYTHON_MODULE(dials_scratch_jmp_sim_ext)
+  {
+    def("compute_profile_internal", &compute_profile_internal);
+  }
+
+}}} // namespace dials_scratch::examples::boost_python
---
+++ jmp/sim/sim.py
@@ -0,0 +1,141 @@
+
+
+def compute_profile(experiments, reflection, reference, N):
+  from dials.array_family import flex
+  from dials.algorithms.profile_model.gaussian_rs import CoordinateSystem
+  from dials.algorithms.profile_model.modeller import GridSampler
+  from dials_scratch.jmp.sim import compute_profile_internal
+  from random import uniform
+
+  sbox = reflection['shoebox']
+  bbox = sbox.bbox
+  zs = sbox.zsize()
+  ys = sbox.ysize()
+  xs = sbox.xsize()
+
+  profile = flex.double(flex.grid(zs, ys, xs))
+
+  m2 = experiments[0].goniometer.get_rotation_axis_datum()
+  s0 = experiments[0].beam.get_s0()
+  s1 = reflection['s1']
+  phi = reflection['xyzcal.mm'][2]
+  detector = experiments[0].detector
+  scan = experiments[0].scan
+
+  cs = CoordinateSystem(m2, s0, s1, phi)
+  
+  scan_range = scan.get_array_range()
+  image_size = detector[0].get_image_size()
+  grid_size = (3, 3, 40)
+  assert grid_size[0]*grid_size[1]*grid_size[2] == len(reference[0])
+
+  sampler = GridSampler(image_size, scan_range, grid_size)
+
+  xyz = reflection['xyzcal.px']
+  index = sampler.nearest(0, xyz)
+
+  for g in reference[0]:
+    assert abs(flex.sum(g) - 1.0) < 1e-7
+
+  grid = reference[0][index]
+
+  sigma_d = experiments[0].profile.sigma_b(deg=False)
+  sigma_m = experiments[0].profile.sigma_m(deg=False)
+  delta_d = 3.0 * sigma_d
+  delta_m = 3.0 * sigma_m
+
+  profile = compute_profile_internal(
+    grid, 
+    bbox, 
+    zs, 
+    ys, 
+    xs, 
+    N, 
+    delta_d, 
+    delta_m, 
+    detector, 
+    scan, 
+    cs)
+
+  # from dials_scratch.jmp.viewer import show_image_stack_multi_view
+  # show_image_stack_multi_view(profile.as_numpy_array(), vmax=max(profile))
+  sum_p = flex.sum(profile)
+  print "Partiality: %f" % sum_p
+  try:
+    assert sum_p > 0, "sum_p == 0" 
+  except Exception, e:
+    print e
+    return None
+
+  return profile
+
+
+
+
+def write_profiles(experiments, reflections, reference, N, filename):
+
+  profiles = []
+
+  for i, refl in enumerate(reflections):
+
+    int_flag = refl['flags'] & reflections.flags.integrated_prf
+
+    if int_flag:
+      print "Computing profile for %d / %d" % (i, len(reflections))
+      s = compute_profile(experiments, refl, reference, N)
+    else:
+      s = None
+
+    profiles.append(s)
+
+  import cPickle as pickle
+  pickle.dump(profiles, open(filename, "w"))
+
+
+
+from libtbx.phil import parse
+
+phil_scope = parse('''
+  
+  experiments = None
+    .type = str
+
+  reflections = NOne
+    .type = str
+
+  reference = None
+    .type = str
+
+  N = 125
+    .type = int
+
+  output = "profile.pickle"
+    .type = str
+
+''')
+
+
+if __name__ == '__main__':
+
+  from dials.array_family import flex
+  from dxtbx.model.experiment_list import ExperimentListFactory
+  from dials.algorithms.integration.report import IntegrationReport
+  import cPickle as pickle
+  import sys
+
+  # Parse command line
+  interpretor = phil_scope.command_line_argument_interpreter()
+  user_phils = interpretor.process_args(sys.argv[1:])
+  working_phil = phil_scope.fetch(sources=user_phils)
+  params = working_phil.extract()
+
+  assert params.experiments is not None
+  assert params.reflections is not None
+  assert params.reference is not None
+
+  # Extract experiments and reflections
+  experiments = ExperimentListFactory.from_json_file(params.experiments)
+  reflections = flex.reflection_table.from_pickle(params.reflections)
+  reference = pickle.load(open(params.reference))
+
+  write_profiles(experiments, reflections, reference, params.N, params.output)
