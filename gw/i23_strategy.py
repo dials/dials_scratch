9e8f66d47ac2be3b3abe0c4fe188cfaf7d3a39da
graeme-winter <graeme.winter@gmail.com>
2017-06-27 15:41:47 +0100

    +- 60 works nice

--- gw/i23_strategy.py
+++ gw/i23_strategy.py
@@ -1,4 +1,25 @@
 from scitbx import matrix
+import sys
+
+def read_expt(filename):
+  from dials.util.options import flatten_datablocks
+  from dials.util.phil import DataBlockConverters
+  converter = DataBlockConverters(check_format=False)
+  return flatten_datablocks([converter.from_string(filename)])
+
+def write_expt(experiments, filename):
+  from dxtbx.datablock import DataBlockDumper
+  dump = DataBlockDumper(experiments)
+  dump.as_file(filename)
+
+expts = read_expt(sys.argv[1])
+expt = expts[0]
+
+sweeps = expt.extract_sweeps()
+
+expt = sweeps[0]
+
+gonio = expt.get_goniometer()
 
 e1 = matrix.col((1, 0, 0))
 e2 = matrix.col((0.6691306063588582, 0.7431448254773942, 0))
@@ -7,15 +28,22 @@ z = matrix.col((0, 0, 1))
 
 from dials.algorithms.refinement import rotation_decomposition
 
-Rplus = z.axis_and_angle_as_r3_rotation_matrix(1.1344640137963142)
-Rminus = z.axis_and_angle_as_r3_rotation_matrix(-1.1344640137963142)
+import math
+a = math.pi / 3
+
+Rplus = z.axis_and_angle_as_r3_rotation_matrix(a)
+Rminus = z.axis_and_angle_as_r3_rotation_matrix(-a)
 
 Splus = rotation_decomposition.solve_r3_rotation_for_angles_given_axes(
   Rplus, e1, e2, e3, return_both_solutions=True, deg=True)
 Sminus = rotation_decomposition.solve_r3_rotation_for_angles_given_axes(
   Rminus, e1, e2, e3, return_both_solutions=True, deg=True)
 
-for s in Splus:
-  print '%7.2f %7.2f %7.2f' % s
-for s in Sminus:
-  print '%7.2f %7.2f %7.2f' % s
+s = Splus[0]
+F = e2.axis_and_angle_as_r3_rotation_matrix(s[1]) * \
+  e3.axis_and_angle_as_r3_rotation_matrix(s[2])
+
+gonio.set_fixed_rotation(F.elems)
+gonio.set_angles(s)
+
+write_expt(expts, sys.argv[2])
