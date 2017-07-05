f1b94dd56924b226b441c12d08c1c946967c7ff6
graeme-winter <graeme.winter@gmail.com>
2017-07-05 13:41:36 +0100

    Clean up code

--- command_line/sniper.py
+++ command_line/sniper.py
@@ -1,27 +1,22 @@
 def allowed(perm):
   m = perm[0] + perm[1]
-  if m != (perm[-2] + perm[-1]):
-    return None
+  if m != (perm[-2] + perm[-1]): return None
   for j in range(1, len(perm) - 1, 2):
-    if sum(perm[j:j+3]) != m:
-      return None
+    if sum(perm[j:j+3]) != m: return None
   return perm
 
-def csolver(n):
+def solver(n):
   import itertools
-  permutations = 0
-  solutions = []
+  perms = 0
+  solns = []
   for perm in itertools.permutations(range(1, 2 * n)):
-    permutations += 1
+    perms += 1
     if allowed(perm):
-      if not tuple(reversed(perm)) in solutions:
-        solutions.append(perm)
-  print 'Of %d permutations %d solutions:' % (permutations, len(solutions))
-  for s in solutions: print s
+      if not tuple(reversed(perm)) in solns: solns.append(perm)
+  return perms, solns
 
 if __name__ == '__main__':
-  import sys, time
-  t0 = time.time()
-  csolver(int(sys.argv[1]))
-  t1 = time.time()
-  print 'Took %.2fs' % (t1 - t0)
+  import sys
+  perms, solns = solver(int(sys.argv[1]))
+  print 'Of %d permutations %d unique solutions:' % (perms, len(solns))
+  for s in solns: print s
