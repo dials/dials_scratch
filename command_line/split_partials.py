95db49abfd8c69318cbf533a0b9344d4829b7932
graeme-winter <graeme.winter@gmail.com>
2017-10-12 06:47:46 +0100

    Jiffy to split partials out for plotting; usage: dials_scratch.split_partials integrated.pickle out97.pickle 0.97 0.025820267 - 0.074528982: 1 0.074528982 - 0.1232377: 0 0.1232377 - 0.17194641: 0 0.17194641 - 0.22065513: 1 0.22065513 - 0.26936384: 0 0.26936384 - 0.31807256: 1 0.31807256 - 0.36678127: 0 0.36678127 - 0.41548999: 0 0.41548999 - 0.4641987: 0 0.4641987 - 0.51290742: 1 0.51290742 - 0.56161613: 1 0.56161613 - 0.61032485: 1 0.61032485 - 0.65903356: 0 0.65903356 - 0.70774228: 3 0.70774228 - 0.75645099: 2 0.75645099 - 0.80515971: 3 0.80515971 - 0.85386842: 5 0.85386842 - 0.90257713: 4 0.90257713 - 0.95128585: 3 0.95128585 - 0.99999456: 741 Wrote 32 reflections with partiality < 0.970 to out97.pickle

---
+++ command_line/split_partials.py
@@ -0,0 +1,27 @@
+from dials.array_family import flex
+import cPickle as pickle
+import sys
+
+refl_in = sys.argv[1]
+refl_out = sys.argv[2]
+part_lim = float(sys.argv[3])
+
+refl = pickle.load(open(refl_in, 'r'))
+
+# remove duff reflections
+sel = refl.get_flags(refl.flags.integrated_sum, all=True)
+refl = refl.select(sel)
+sel = refl.get_flags(refl.flags.integrated, all=True)
+refl = refl.select(sel)
+
+# extract partiality, plot histogram, select subset, dump
+part = refl['partiality']
+parth = flex.histogram(part, n_slots=20)
+parth.show()
+edge = part < part_lim
+edge_refl = refl.select(edge)
+
+pickle.dump(edge_refl, open(refl_out, 'w'))
+
+print 'Wrote %d reflections with partiality < %.3f to %s' % \
+  (len(edge_refl), part_lim, refl_out)
