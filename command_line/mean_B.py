69775280b50b1e7add284272c83430b47add62ea
graeme-winter <graeme.winter@gmail.com>
2017-10-26 12:22:50 +0100

    Strip out Uij==0 for H; compute B for H & not-H

--- command_line/mean_B.py
+++ command_line/mean_B.py
@@ -10,6 +10,14 @@ def get_B(cif_file):
       name = record.strip().replace('data_', '')
       break
 
+  # strip out Uij == 0 for H atoms, then rebuild cif_text
+  non_zero = []
+  for record in cif_text.split('\n'):
+    if record.startswith('H') and '0 0 0 0 0 0' in record:
+      continue
+    non_zero.append(record)
+  cif_text = '\n'.join(non_zero)
+
   if name is '':
     cif_text = cif_text.replace('data_', 'data_35dnba')
     name = '35dnba'
@@ -17,9 +25,14 @@ def get_B(cif_file):
   # read structure, remove hydrogen, make isotropic for analysis
   cs = iotbx.cif.reader(input_string=cif_text).build_crystal_structures()[name]
   csh = cs.element_selection('H')
+
+  hs = cs.select(csh)
+  hs.convert_to_isotropic()
+
   cs = cs.select(~csh)
   cs.convert_to_isotropic()
-  return cs.b_iso_min_max_mean()[2]
+
+  return cs.b_iso_min_max_mean()[2], hs.b_iso_min_max_mean()[2]
 
 if __name__ == '__main__':
   import sys
@@ -29,4 +42,4 @@ if __name__ == '__main__':
     bs[filename] = get_B(filename)
 
   for filename in sorted(bs):
-    print filename, bs[filename]
+    print filename, '%.4f %.4f' % bs[filename]
