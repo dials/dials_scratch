a357edc11d74d51723ef2a8e4cf19f53358a4f2c
James Parkhurst <james.parkhurst@diamond.ac.uk>
2018-02-05 11:00:15 +0000

    Fixed volume monte carlo test

--- jmp/pixel_volume/test_pixel_volume.py
+++ jmp/pixel_volume/test_pixel_volume.py
@@ -22,7 +22,7 @@ def monte_carlo_pixel_volume(experiment, i, j):
   m2 = matrix.col(goniometer.get_rotation_axis_datum())
   s0 = matrix.col(beam.get_s0())
 
-  N = 1000000
+  N = 100000
 
   s00 = matrix.col(panel.get_pixel_lab_coord((i,j))).normalize() * s0_length
   s01 = matrix.col(panel.get_pixel_lab_coord((i+1,j))).normalize() * s0_length
@@ -67,7 +67,7 @@ def monte_carlo_pixel_volume(experiment, i, j):
     elif abs(angle - pi) < 1e-7:
       s = -x
     else:
-      s = x.rotate_around_origin(axis=axis, angle=angle) * scc.length()
+      s = x.rotate_around_origin(axis=axis, angle=angle, deg=False) * scc.length()
 
     px, py = panel.get_ray_intersection_px(s)
 
@@ -81,6 +81,198 @@ def monte_carlo_pixel_volume(experiment, i, j):
 
   V = box_area * count / float(N)
 
+  return V
+
+def monte_carlo_pixel_volume2(experiment, i, j):
+
+  from dials.array_family import flex
+  from scitbx import matrix
+  from random import uniform
+  from math import acos, pi, cos, sin, floor, sqrt, atan2
+  from dials.algorithms.spot_prediction import RotationAngles
+
+  beam = experiment.beam
+  goniometer = experiment.goniometer
+  panel = experiment.detector[0]
+  scan = experiment.scan
+
+  s0_length = 1.0 / beam.get_wavelength()
+
+  phi0, phi1 = scan.get_oscillation(deg=False)
+
+  DPHI = phi1
+
+  m2 = matrix.col(goniometer.get_rotation_axis_datum())
+  s0 = matrix.col(beam.get_s0())
+
+  N = 100000
+
+  s00 = matrix.col(panel.get_pixel_lab_coord((i,j))).normalize() * s0_length
+  s01 = matrix.col(panel.get_pixel_lab_coord((i+1,j))).normalize() * s0_length
+  s10 = matrix.col(panel.get_pixel_lab_coord((i,j+1))).normalize() * s0_length
+  s11 = matrix.col(panel.get_pixel_lab_coord((i+1,j+1))).normalize() * s0_length
+  scc = matrix.col(panel.get_pixel_lab_coord((i+0.5,j+0.5))).normalize() * s0_length
+
+  p0_00 = s00 - s0
+  p0_01 = s01 - s0
+  p0_10 = s10 - s0
+  p0_11 = s11 - s0
+
+  p1_00 = p0_00.rotate_around_origin(axis=m2, angle=-DPHI, deg=False)
+  p1_01 = p0_01.rotate_around_origin(axis=m2, angle=-DPHI, deg=False)
+  p1_10 = p0_10.rotate_around_origin(axis=m2, angle=-DPHI, deg=False)
+  p1_11 = p0_11.rotate_around_origin(axis=m2, angle=-DPHI, deg=False)
+
+  X0, Y0, Z0 = zip(*[p0_00, p0_01, p0_10, p0_11])
+  X1, Y1, Z1 = zip(*[p1_00, p1_01, p1_10, p1_11])
+
+  XX, YY, ZZ = X0 + X1, Y0 + Y1, Z0 + Z1
+  Xmin, Xmax = min(XX), max(XX)
+  Ymin, Ymax = min(YY), max(YY)
+  Zmin, Zmax = min(ZZ), max(ZZ)
+
+  Xrange = (Xmax - Xmin) * 1.1
+  Yrange = (Ymax - Ymin) * 1.1
+  Zrange = (Zmax - Zmin) * 1.1
+  Xmid = (Xmax + Xmin) / 2.0
+  Ymid = (Ymax + Ymin) / 2.0
+  Zmid = (Zmax + Zmin) / 2.0
+
+  Xmin = Xmid - Xrange / 2.0
+  Ymin = Ymid - Yrange / 2.0
+  Zmin = Zmid - Zrange / 2.0
+  Xmax = Xmid + Xrange / 2.0
+  Ymax = Ymid + Yrange / 2.0
+  Zmax = Zmid + Zrange / 2.0
+
+  count = 0
+  min_angle = 10
+  max_angle = -10
+  X = []
+  Y = []
+  Z = []
+  for n in range(N):
+    x = uniform(Xmin, Xmax)
+    y = uniform(Ymin, Ymax)
+    z = uniform(Zmin, Zmax)
+
+    r0 = matrix.col((x, y, z))
+
+    try:
+      angles = RotationAngles(s0, m2)(r0)
+    except Exception:
+      continue
+
+    for angle in angles:
+      #print angle, DPHI
+      if angle >= 0 and angle <= DPHI:
+        if angle > max_angle:
+          max_angle = angle
+        if angle < min_angle:
+          min_angle = angle
+        p = r0.rotate_around_origin(axis=m2, angle=angle, deg=False)
+        s = s0 + p
+        px, py = panel.get_ray_intersection_px(s)
+
+        px, py = int(floor(px)), int(floor(py))
+        #print px, py, i, j
+        if px == i and py == j:
+          count += 1
+          X.append(x)
+          Y.append(y)
+          Z.append(z)
+
+  box_volume = (Xmax - Xmin)*(Ymax-Ymin)*(Zmax-Zmin)
+  V = box_volume * count / float(N)
+  # print box_volume, V, count, min_angle * 180.0 / pi, max_angle*180.0 / pi, DPHI*180.0 /pi
+
+  # from mpl_toolkits.mplot3d import Axes3D
+  # from matplotlib import pylab
+  # fig = pylab.figure()
+  # ax = fig.add_subplot(111, projection='3d')
+  # ax.scatter(X, Y, Z, color="blue")
+
+  # # Draw box around volume
+  # ax.plot3D([Xmin, Xmax], [Ymin, Ymin], [Zmin, Zmin], color="red")
+  # ax.plot3D([Xmin, Xmax], [Ymax, Ymax], [Zmin, Zmin], color="red")
+  # ax.plot3D([Xmin, Xmax], [Ymin, Ymin], [Zmax, Zmax], color="red")
+  # ax.plot3D([Xmin, Xmax], [Ymax, Ymax], [Zmax, Zmax], color="red")
+  # ax.plot3D([Xmin, Xmin], [Ymin, Ymax], [Zmin, Zmin], color="red")
+  # ax.plot3D([Xmax, Xmax], [Ymin, Ymax], [Zmin, Zmin], color="red")
+  # ax.plot3D([Xmin, Xmin], [Ymin, Ymax], [Zmax, Zmax], color="red")
+  # ax.plot3D([Xmax, Xmax], [Ymin, Ymax], [Zmax, Zmax], color="red")
+  # ax.plot3D([Xmin, Xmin], [Ymin, Ymin], [Zmin, Zmax], color="red")
+  # ax.plot3D([Xmax, Xmax], [Ymin, Ymin], [Zmin, Zmax], color="red")
+  # ax.plot3D([Xmin, Xmin], [Ymax, Ymax], [Zmin, Zmax], color="red")
+  # ax.plot3D([Xmax, Xmax], [Ymax, Ymax], [Zmin, Zmax], color="red")
+
+  # # Draw R0 and R0 rotated
+
+
+  # pylab.show()
+
+  # x00, y00, z00 = p0_00
+  # x01, y01, z01 = p0_01
+  # x10, y10, z10 = p0_10
+  # x11, y11, z11 = p0_11
+
+  # x00, y00, z00 = p0_00
+  # x01, y01, z01 = p0_01
+  # x10, y10, z10 = p0_10
+  # x11, y11, z11 = p0_11
+
+
+#
+  #R00 = sqrt(z00**2 + y00**2)
+  #R01 = sqrt(z01**2 + y01**2)
+  #R10 = sqrt(z10**2 + y10**2)
+  #R11 = sqrt(z11**2 + y11**2)
+
+  #P00 = atan2(z00, y00)
+  #P01 = atan2(z01, y01)
+  #P10 = atan2(z10, y10)
+  #P11 = atan2(z11, y11)
+
+  #xmin, xmax = min(x00, x01, x10, x11), max(x00, x01, x10, x11)
+  #Rmin, Rmax = min(R00, R01, R10, R11), max(R00, R01, R10, R11)
+  #Pmin, Pmax = min(P00, P01, P10, P11), max(P00, P01, P10, P11)
+
+  ## print xmin, xmax, Rmin, Rmax, Pmin, Pmax, DPHI
+
+  #Pmax += DPHI
+  #count = 0
+  #min_angle = 10
+  #max_angle = -10
+  #for n in range(N):
+
+  #  x = uniform(xmin, xmax)
+  #  R = uniform(Rmin, Rmax)
+  #  P = uniform(Pmin, Pmax)
+
+  #  r0 = matrix.col((x, R*cos(P), R*sin(P)))
+  #  # print r0.angle(scc - s0, deg=True)
+  #  angles = RotationAngles(s0, m2)(r0)
+  #  print P, angles
+  #  for angle in angles:
+  #    #print angle, DPHI
+  #    if angle >= 0 and angle <= DPHI:
+  #      if angle > max_angle:
+  #        max_angle = angle
+  #      if angle < min_angle:
+  #        min_angle = angle
+  #      p = r0.rotate_around_origin(axis=m2, angle=angle, deg=False)
+  #      s = s0 + p
+  #      px, py = panel.get_ray_intersection_px(s)
+
+  #      px, py = int(floor(px)), int(floor(py))
+  #      #print px, py, i, j
+  #      if px == i and py == j:
+  #        count += 1
+
+
+  ##box_area = 2*pi*s0.length()**2 * (1-cos(phi_max))
+  #box_volume = (Rmax**2 - Rmin**2) * (Pmax - Pmin)*(xmax-xmin)/2.0
+  ##print box_volume, count, N
   return V
 
 
@@ -98,9 +290,9 @@ def pixel_polygon_area(experiment, i, j):
 
   s0_length = 1.0 / beam.get_wavelength()
 
-  phi0, phi1 = scan.get_oscillation()
+  phi0, phi1 = scan.get_oscillation(deg=False)
 
-  DPHI = phi1 - phi0
+  DPHI = phi1
 
   m2 = matrix.col(goniometer.get_rotation_axis_datum())
   s0 = matrix.col(beam.get_s0())
@@ -120,7 +312,11 @@ def pixel_polygon_area(experiment, i, j):
 
   A = ((A-B).cross(A-D)).length() / 2.0 + ((C-B).cross(C-D)).length() / 2.0
 
-  return A
+  Linv = m2.dot(s1.cross(s0)) / s0_length**2
+
+  V = abs(A * DPHI * Linv * s0_length)
+
+  return V
 
 def pixel_volume(experiment, i, j):
 
@@ -136,9 +332,9 @@ def pixel_volume(experiment, i, j):
 
   s0_length = 1.0 / beam.get_wavelength()
 
-  phi0, phi1 = scan.get_oscillation()
+  phi0, phi1 = scan.get_oscillation(deg=False)
 
-  DPHI = phi1 - phi0
+  DPHI = phi1
 
   m2 = matrix.col(goniometer.get_rotation_axis_datum())
   s0 = matrix.col(beam.get_s0())
@@ -193,11 +389,11 @@ def pixel_volume(experiment, i, j):
   A2 = (Aa + Ba + Ca - pi)*s0_length**2
 
   A = A1+A2
-  #Linv = m2.dot(s1.cross(s0)) / s0_length**2
+  Linv = m2.dot(s1.cross(s0)) / s0_length**2
 
-  #V = abs(A * DPHI * Linv * s0_length)
+  V = abs(A * DPHI * Linv * s0_length)
 
-  return A
+  return V
 
 
 
@@ -213,10 +409,11 @@ def monte_carlo_pixel_volume_all_pixels(experiment):
     i = randint(0, xsize-1)
     j = randint(0, ysize-1)
     st = time()
-    V = monte_carlo_pixel_volume(experiment, i, j)
+    V = monte_carlo_pixel_volume2(experiment, i, j)
     V1 = pixel_polygon_area(experiment, i, j)
     V2 = pixel_volume(experiment, i, j)
     print i, j, V, V1, V2, time() - st
+    # exit(0)
     volume[j,i] = V
 
   return volume
