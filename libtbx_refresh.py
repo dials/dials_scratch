aa38c15ac04165e6b1d38498b7bd57e10fc82344
James Beilsten-Edmands <jbeilstenedmands@gmail.com>
2018-03-09 17:14:06 +0000

    Renaming of files and start of curvatures calculation.
    Updates to allow use of curvatures if implemented in future.
    
    Renaming models.
    
    Correcting filenames.

--- libtbx_refresh.py
+++ libtbx_refresh.py
@@ -4,7 +4,7 @@ import libtbx.pkg_utils
 
 libtbx.pkg_utils.define_entry_points({
   'dxtbx.scaling_model_ext': [
-  'aimless = dials_scratch.jbe.scaling_code.model.scaling_model_ext:AimlessScalingModelExt',
+  'physical = dials_scratch.jbe.scaling_code.model.scaling_model_ext:PhysicalScalingModelExt',
   'KB = dials_scratch.jbe.scaling_code.model.scaling_model_ext:KBScalingModelExt',
-  'xscale = dials_scratch.jbe.scaling_code.model.scaling_model_ext:XscaleScalingModelExt'
+  'array = dials_scratch.jbe.scaling_code.model.scaling_model_ext:ArrayScalingModelExt'
   ]})
