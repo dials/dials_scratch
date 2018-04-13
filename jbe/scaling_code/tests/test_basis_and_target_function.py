fad72589ca9372dd3d6517249b1a9c44964cdb69
James Beilsten-Edmands <jbeilstenedmands@gmail.com>
2018-04-13 14:37:09 +0100

    Reworking Ih_table, scaler to not require ordered reflection table.

--- jbe/scaling_code/tests/test_basis_and_target_function.py
+++ jbe/scaling_code/tests/test_basis_and_target_function.py
@@ -15,7 +15,7 @@ from dials_scratch.jbe.scaling_code.model.scaling_model_factory import \
   create_scaling_model
 from dials_scratch.jbe.scaling_code.scaler_factory import create_scaler
 from dials_scratch.jbe.scaling_code.target_function import ScalingTarget
-from basis_functions import basis_function
+from dials_scratch.jbe.scaling_code.basis_functions import basis_function
 from dials_scratch.jbe.scaling_code.model.components.scale_components import \
   SingleBScaleFactor, SingleScaleFactor
 
@@ -188,7 +188,7 @@ def test_target_function(generated_KB_param):
   #_ = target.compute_functional_gradients_and_curvatures()
 
   # Calculate residuals explicitly and check RMSDS.
-  assert approx_equal(list(resid), [0.0, 50.0/36.0, 100.0/36.0])
+  assert approx_equal(list(resid), [50.0/36.0, 0.0, 100.0/36.0])
   assert approx_equal(target.rmsds()[0], (150.0/(36.0*3.0))**0.5)
 
 
