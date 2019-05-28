752afb686b51e6550782a11bffeff7792b0dad78
James Beilsten-Edmands <jbeilstenedmands@gmail.com>
2019-05-28 13:26:45 +0100

    Split plotting script into extract and plot scripts

---
+++ jbe/plotting/extract_incremental_stats.py
@@ -0,0 +1,63 @@
+import gc
+import resource
+import json
+from dxtbx.serialize import load
+from dials.array_family import flex
+from dials.algorithms.scaling.scaling_library import scaled_data_as_miller_array
+from dials.report.plots import ResolutionPlotsAndStats
+import iotbx
+
+
+def merging_stats_from_scaled_array(scaled_miller_array):
+    result = iotbx.merging_statistics.dataset_statistics(
+        i_obs=scaled_miller_array,
+        n_bins=20,
+        anomalous=False,
+        sigma_filtering=None,
+        eliminate_sys_absent=False,
+        use_internal_variance=False,
+        cc_one_half_significance_level=0.01,
+    )
+
+    intensities_anom = scaled_miller_array.as_anomalous_array()
+    intensities_anom = intensities_anom.map_to_asu().customized_copy(
+        info=scaled_miller_array.info()
+    )
+    anom_result = iotbx.merging_statistics.dataset_statistics(
+        i_obs=intensities_anom,
+        n_bins=20,
+        anomalous=True,
+        sigma_filtering=None,
+        cc_one_half_significance_level=0.01,
+        eliminate_sys_absent=False,
+        use_internal_variance=False,
+    )
+    return result, anom_result
+
+filenames = (
+    "/Users/whi10850/Documents/test_data/multi_example/integrated_files/inc_test/1/",
+    "/Users/whi10850/Documents/test_data/multi_example/integrated_files/inc_test/2/",
+)
+
+cc_one_half_data = []
+i_over_sig_data = []
+
+for filepath in filenames:
+    experiments = load.experiment_list(filepath+"scaled_experiments.json", check_format=False)
+    reflections = flex.reflection_table.from_pickle(filepath+"scaled.pickle")
+    print("memory: %s" % int(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss))
+    arr = scaled_data_as_miller_array([reflections], experiments)
+    norm_stats, anom_stats = merging_stats_from_scaled_array(arr)
+    plotter = ResolutionPlotsAndStats(norm_stats, anom_stats)
+    resolution_plots = plotter.make_all_plots()
+    cc_one_half_data.append(resolution_plots["cc_one_half"])
+    i_over_sig_data.append(resolution_plots["i_over_sig_i"])
+    del experiments
+    del reflections
+    gc.collect()
+    print("added data for %s" % filepath)
+
+
+data = {"cc_one_half" : cc_one_half_data, "i_over_sigma": i_over_sig_data}
+with open("incremental_data.json", "w") as f:
+    json.dump(data, f, indent=True)
--- jbe/plotting/plot_incremental_stats.py
+++ jbe/plotting/plot_incremental_stats.py
@@ -1,77 +1,34 @@
+import json
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
-from dxtbx.serialize import load
-from dials.array_family import flex
-from dials.algorithms.scaling.scaling_library import scaled_data_as_miller_array
-from dials.report.plots import ResolutionPlotsAndStats
-import iotbx
+matplotlib.use("Agg")
 
-def merging_stats_from_scaled_array(scaled_miller_array):
-    result = iotbx.merging_statistics.dataset_statistics(
-        i_obs=scaled_miller_array,
-        n_bins=20,
-        anomalous=False,
-        sigma_filtering=None,
-        eliminate_sys_absent=False,
-        use_internal_variance=False,
-        cc_one_half_significance_level=0.01,
-    )
+with open("incremental_data.json") as f:
+    data = json.load(f)
+    cc_one_half_data = data["cc_one_half"]
+    i_over_sig_data = data["i_over_sigma"]
 
-    intensities_anom = scaled_miller_array.as_anomalous_array()
-    intensities_anom = intensities_anom.map_to_asu().customized_copy(
-        info=scaled_miller_array.info()
-    )
-    anom_result = iotbx.merging_statistics.dataset_statistics(
-        i_obs=intensities_anom,
-        n_bins=20,
-        anomalous=True,
-        sigma_filtering=None,
-        cc_one_half_significance_level=0.01,
-        eliminate_sys_absent=False,
-        use_internal_variance=False,
-    )
-    return result, anom_result
+    fontsize = 10
 
-filenames = (
-    "/Users/whi10850/Documents/test_data/multi_example/integrated_files/inc_test/1/",
-    "/Users/whi10850/Documents/test_data/multi_example/integrated_files/inc_test/2/",
-)
+    fig = plt.figure(figsize=(10, 4.5))
+    gs = GridSpec(1, 2)
+    ax1 = fig.add_subplot(gs[0, 0])
+    for i, d in enumerate(cc_one_half_data):
+        ax1.plot(d["data"][0]["x"], d["data"][0]["y"], label=r"CC$_{1/2}$"+" (%s)" % i)
+        ax1.plot(d["data"][2]["x"], d["data"][2]["y"], label=r"CC$_{1/2}$-anom."+" (%s)" % i)
+    ax1.set_xticks(cc_one_half_data[-1]["layout"]["xaxis"]["tickvals"])
+    ax1.set_xticklabels(cc_one_half_data[-1]["layout"]["xaxis"]["ticktext"])
+    ax1.legend(fontsize=fontsize)
+    ax1.set_xlabel(r"Resolution ($\AA$)", fontsize=fontsize)
+    ax1.set_ylabel(r"CC$_{1/2}$", fontsize=fontsize)
 
-cc_one_half_data = []
-i_over_sig_data = []
+    ax2 = fig.add_subplot(gs[0, 1])
+    for i, d in enumerate(i_over_sig_data):
+        ax2.plot(d["data"][0]["x"], d["data"][0]["y"], label="I / sigma (%s)" % i)
+    ax2.set_xticks(i_over_sig_data[-1]["layout"]["xaxis"]["tickvals"])
+    ax2.set_xticklabels(i_over_sig_data[-1]["layout"]["xaxis"]["ticktext"])
+    ax2.set_xlabel(r"Resolution ($\AA$)", fontsize=fontsize)
+    ax2.set_ylabel(r"< I / $\sigma$ >", fontsize=fontsize)
+    ax2.legend(fontsize=fontsize)
 
-for filepath in filenames:
-    experiments = load.experiment_list(filepath+"scaled_experiments.json", check_format=False)
-    reflections = flex.reflection_table.from_pickle(filepath+"scaled.pickle")
-
-    arr = scaled_data_as_miller_array([reflections], experiments)
-    norm_stats, anom_stats = merging_stats_from_scaled_array(arr)
-    plotter = ResolutionPlotsAndStats(norm_stats, anom_stats)
-    resolution_plots = plotter.make_all_plots()
-    cc_one_half_data.append(resolution_plots["cc_one_half"]["data"])
-    i_over_sig_data.append(resolution_plots["i_over_sig_i"]["data"])
-
-fontsize = 10
-
-fig = plt.figure(figsize=(10, 4.5))
-gs = GridSpec(1, 2)
-ax1 = fig.add_subplot(gs[0, 0])
-for i, d in enumerate(cc_one_half_data):
-    ax1.plot(d[0]["x"], d[0]["y"], label=r"CC$_{1/2}$"+" (%s)" % i)
-    ax1.plot(d[2]["x"], d[2]["y"], label=r"CC$_{1/2}$-anom."+" (%s)" % i)
-ax1.set_xticks(resolution_plots["cc_one_half"]["layout"]["xaxis"]["tickvals"])
-ax1.set_xticklabels(resolution_plots["cc_one_half"]["layout"]["xaxis"]["ticktext"])
-ax1.legend(fontsize=fontsize)
-ax1.set_xlabel(r"Resolution ($\AA$)", fontsize=fontsize)
-ax1.set_ylabel(r"CC$_{1/2}$", fontsize=fontsize)
-
-ax2 = fig.add_subplot(gs[0, 1])
-for i, d in enumerate(i_over_sig_data):
-    ax2.plot(d[0]["x"], d[0]["y"], label="I / sigma (%s)" % i)
-ax2.set_xticks(resolution_plots["i_over_sig_i"]["layout"]["xaxis"]["tickvals"])
-ax2.set_xticklabels(resolution_plots["i_over_sig_i"]["layout"]["xaxis"]["ticktext"])
-ax2.set_xlabel(r"Resolution ($\AA$)", fontsize=fontsize)
-ax2.set_ylabel(r"< I / $\sigma$ >", fontsize=fontsize)
-ax2.legend(fontsize=fontsize)
-
-plt.savefig("incremental_stats.pdf")
+    plt.savefig("incremental.pdf")
