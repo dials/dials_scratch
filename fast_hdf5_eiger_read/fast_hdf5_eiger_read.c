529e4824ad6af332e7da69ec9ca8d1a42ce5a26f
Graeme Winter <graeme.winter@gmail.com>
2019-09-22 16:24:30 +0100

    Make it threaded, seems to work fine

--- fast_hdf5_eiger_read/fast_hdf5_eiger_read.c
+++ fast_hdf5_eiger_read/fast_hdf5_eiger_read.c
@@ -1,30 +1,100 @@
 /* hello from 1990 - please compile with h5cc -o fast_hdf5_eiger_read.c  */
 
-#include <time.h>
 #include <hdf5.h>
 #include <hdf5_hl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include "bitshuffle.h"
 
-int decompress(char * buffer, void * dest, size_t size, size_t element)
-{
-  // the data are 12 bytes in...
-  return bshuf_decompress_lz4(buffer+12, (void *) dest, size, element, 0);
+pthread_mutex_t hdf_mutex;
+
+int n_jobs;
+int job;
+
+hid_t dataset, datasize;
+hsize_t dims[3];
+
+typedef struct chunk_t {
+  char * chunk;
+  size_t chunk_size;
+  int index;
+} chunk_t;
+
+chunk_t next() {
+  hsize_t offset[3];
+
+  chunk_t retval;
+
+  pthread_mutex_lock(&hdf_mutex);
+
+  offset[0] = job;
+  offset[1] = 0;
+  offset[2] = 0;
+  job += 1;
+
+  if(job > n_jobs) {
+    pthread_mutex_unlock(&hdf_mutex);
+    retval.chunk_size = 0;
+    retval.chunk = NULL;
+    retval.index = 0;
+    return retval;
+  }
+
+  hsize_t chunk_size;
+
+  H5Dget_chunk_storage_size(dataset, offset, &chunk_size);
+  retval.chunk_size = chunk_size;
+  retval.index = offset[0];
+
+  retval.chunk = (char *) malloc(chunk_size);
+
+  uint32_t filter = 0;
+  int status = H5DOread_chunk(dataset, H5P_DEFAULT, offset, &filter,
+                              retval.chunk);
+  pthread_mutex_unlock(&hdf_mutex);
+  return retval;
 }
-  
+
+void* worker(void * nonsense) {
+  int size = dims[1] * dims[2];
+  char* buffer = (char *) malloc(datasize * size);
+  int32_t * longbuffer = (int32_t *) buffer;
+  int16_t * shortbuffer = (int16_t *) buffer;
+
+  while(1) {
+    chunk_t chunk = next();
+    if (chunk.chunk_size == 0) {
+      free(buffer);
+      return NULL;
+    }
+    bshuf_decompress_lz4((chunk.chunk)+12, (void *) buffer, size, datasize, 0);
+
+    int64_t total = 0;
+
+    for (size_t j = 0; j < size; j++) {
+      if (datasize == 2) {
+        if (shortbuffer[j] > 0) total += shortbuffer[j];
+      } else {
+        if (longbuffer[j] > 0) total += longbuffer[j];
+      }
+    }
+    printf("Total counts for frame %d %lld\n", chunk.index, total);
+    free(chunk.chunk);
+  }
+  return NULL;
+}
+
 int main(int argc,
-	 char ** argv)
+         char ** argv)
 {
-  hid_t file, dataset, plist, space, memory, datatype, datasize;
+  hid_t file, plist, space, datatype;
   H5D_layout_t layout;
   herr_t status;
 
-  hsize_t dims[3], offset[3], block[3];
-  
   int * buffer = 0;
   char * chunk_buffer = 0;
-  
+
   file = H5Fopen(argv[1], H5F_ACC_RDONLY, H5P_DEFAULT);
   dataset = H5Dopen(file, argv[2], H5P_DEFAULT);
   plist = H5Dget_create_plist(dataset);
@@ -33,7 +103,7 @@ int main(int argc,
   layout = H5Pget_layout(plist);
 
   printf("Element size %lld\n", datasize);
-  
+
   if (layout != H5D_CHUNKED) {
     printf("You will not go to space, sorry\n");
     H5Pclose(plist);
@@ -41,9 +111,9 @@ int main(int argc,
     H5Fclose(file);
     return 1;
   }
-  
+
   space = H5Dget_space(dataset);
-  
+
   printf("N dimensions %d\n", H5Sget_simple_extent_ndims(space));
 
   H5Sget_simple_extent_dims(space, dims, NULL);
@@ -51,64 +121,31 @@ int main(int argc,
   for (int j = 0; j < 3; j++) {
     printf("Dimension %d: %lld\n", j, dims[j]);
   }
-  
-  buffer = (int *) malloc (sizeof(int) * dims[1] * dims[2]);
 
-  block[0] = 1;
-  block[1] = dims[1];
-  block[2] = dims[2];
-  offset[0] = 0;
-  offset[1] = 0;
-  offset[2] = 0; 
-
-  uint64_t total = 0;
   size_t size = dims[1] * dims[2];
 
-  
-  void * frame_buffer = malloc (datasize * size);
+  job = 0;
+  n_jobs = dims[0];
 
-  double read_time = 0.0;
-  double decompress_time = 0.0;
+  /* allocate and spin up threads */
 
-  clock_t start, end;
-  
-  for (int j = 0; j < dims[0]; j++) {
-    printf(".");
-    fflush(stdout);
-    hsize_t chunk_size;
-    total += chunk_size;
-    offset[0] = j;
+  int n_threads = atoi(argv[3]);
+  pthread_t * threads;
 
-    start = clock();
-    H5Dget_chunk_storage_size(dataset, offset, &chunk_size);
+  pthread_mutex_init(&hdf_mutex, NULL);
 
-    chunk_buffer = (char *) malloc (chunk_size);
+  threads = (pthread_t *) malloc(sizeof(pthread_t) * n_threads);
 
-    uint32_t filter = 0;
-    status = H5DOread_chunk(dataset, H5P_DEFAULT, offset,
-			    &filter, chunk_buffer);
-    end = clock();
-    read_time += (double) (end - start) / CLOCKS_PER_SEC;
-
-    start = clock();
-    decompress(chunk_buffer, frame_buffer, size, datasize);
-    end = clock();
-    decompress_time += (double) (end - start) / CLOCKS_PER_SEC;
-    
-    free(chunk_buffer);
-    chunk_buffer = 0;
+  for (int j = 0; j < n_threads; j++) {
+    pthread_create(&threads[j], NULL, worker, NULL);
   }
 
-  free(frame_buffer);
+  for (int j = 0; j < n_threads; j++) {
+    pthread_join(threads[j], NULL);
+  }
 
-  printf("\n");
-  printf("Read %lld images %lld bytes\n", dims[0], total);
-  printf("Total read time: %.2fs\n", read_time);
-  printf("Total decompress time: %.2fs\n", decompress_time);
-  
-  if (chunk_buffer) free(chunk_buffer);
-  if (buffer) free(buffer);
-  
+  pthread_mutex_destroy(&hdf_mutex);
+
   H5Sclose(space);
   status = H5Pclose(plist);
   status = H5Dclose(dataset);
