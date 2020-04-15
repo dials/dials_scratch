357acac6b71ee3b08f087d92168e83e08216fbfa
Graeme Winter <graeme.winter@gmail.com>
2020-04-15 20:52:29 +0100

    Cleaned up / refactored example scripts

---
+++ gw/object_store/digest.py
@@ -0,0 +1,88 @@
+import minio
+import sys
+import ast
+import numpy
+import bitshuffle
+
+from dials.array_family import flex
+from dials.algorithms.spot_finding.threshold import DispersionThresholdStrategy
+from dials.model.data import PixelList, PixelListLabeller
+
+
+def image_to_shoeboxes(image):
+    """For a given image, find spots 2 - 100 pixels im size, assuming a gain
+    of 1, return the list of spot shoeboxes. Also assumes valid intensities in
+    range 0...N."""
+
+    thresholder = DispersionThresholdStrategy(gain=1)
+
+    mask = image.as_1d() >= 0
+    mask.reshape(flex.grid(*image.focus()))
+
+    threshold_mask = thresholder(image, mask=mask)
+    plist = PixelList(0, image, threshold_mask)
+
+    pixel_labeller = PixelListLabeller()
+    pixel_labeller.add(plist)
+
+    creator = flex.PixelListShoeboxCreator(pixel_labeller, 0, 0, True, 2, 100, False)
+    shoeboxes = creator.result()
+    return shoeboxes
+
+
+class Digester(object):
+    def __init__(self, minio_server, username, password):
+        self._client = minio.Minio(
+            "localhost:9000", access_key=username, secret_key=password, secure=False
+        )
+
+    def chunk_to_image(self, chunk):
+        """Decompress chunk to image."""
+
+        blob = numpy.fromstring(chunk[12:], dtype=numpy.uint8)
+        image = bitshuffle.decompress_lz4(blob, self._dim, self._dtype)
+        image = flex.int(image.astype("int32"))
+        return image
+
+    def digest(self, bucket):
+        """Recover the data from bucket, header first then find spots on all
+        the image data found therein."""
+
+        # recover header package & unpack
+
+        header_text = self._client.get_object(bucket, "header").read()
+        self._header = ast.literal_eval(header_text.decode("utf-8"))
+        self._dtype = numpy.dtype("uint%d" % self._header["bit_depth_image"])
+        self._dim = (
+            self._header["y_pixels_in_detector"],
+            self._header["x_pixels_in_detector"],
+        )
+
+        result = {}
+
+        for item in self._client.list_objects(bucket):
+            if item.object_name == "header":
+                continue
+            image_number = int(item.object_name)
+            print(item.object_name)
+            chunk = self._client.get_object(item.bucket_name, item.object_name).read()
+            image = self.chunk_to_image(chunk)
+            shoeboxes = image_to_shoeboxes(image)
+            result[image_number] = len(shoeboxes)
+
+        return result
+
+
+def digest(dcid):
+    digester = Digester("localhost:9000", "hello", "k1tty-pass")
+    result = digester.digest(str(dcid))
+
+    for image_number in sorted(result):
+        print(image_number, result[image_number])
+
+
+if __name__ == "__main__":
+    import sys
+
+    dcid = int(sys.argv[1])
+    digest(dcid)
---
+++ gw/object_store/ingest.py
@@ -0,0 +1,77 @@
+import minio
+import io
+import h5py
+import sys
+import time
+
+
+class Ingester(object):
+    def __init__(self, minio_server, username, password):
+        self._client = minio.Minio(
+            "localhost:9000", access_key=username, secret_key=password, secure=False
+        )
+
+    def ingest(self, master, meta, bucket):
+        """Ingest the data from master h5, with the metadata pulled from meta h5
+        (which is in the /config string dataset for Diamond HDF5 data) and
+        move it into the minio server - the header goes in as "header" with the
+        images numbered from 1 as "000001" etc."""
+
+        # FIXME add error trapping etc.
+        self._client.make_bucket(bucket, location="right-here-1")
+
+        with h5py.File(meta, "r") as f:
+            config = f["config"][()]
+
+        etag = self._client.put_object(
+            bucket, "header", io.BytesIO(config), len(config)
+        )
+
+        # now iterate through the chunks in the actual data set - assumed to
+        # be stored in external data files
+
+        with h5py.File(master, "r") as f:
+            frame_index = 0
+            data = f["/entry/data"]
+            total_read = 0
+            t0 = time.time()
+
+            # assume all the "real" data in data_(something) datasets
+            for k in sorted(d for d in data if d.startswith("data_")):
+                d = data[k]
+                d_id = d.id
+
+                # getting the dataset shape should
+                chunks = d.shape[0]
+
+                # iterate through the chunks found in here and push each up
+                for j in range(chunks):
+                    offset = (j, 0, 0)
+                    filter_mask, chunk = d_id.read_direct_chunk(offset)
+                    chunk_size = len(chunk)
+
+                    # increment counter - we push this in people numbers
+                    frame_index += 1
+
+                    # push chunk to object store
+                    etag = self._client.put_object(
+                        bucket, "%06d" % frame_index, io.BytesIO(chunk), chunk_size
+                    )
+                    total_read += chunk_size
+        t1 = time.time()
+        return total_read, t1 - t0
+
+
+def ingest(master, dcid):
+    ingester = Ingester("localhost:9000", "hello", "k1tty-pass")
+    r, t = ingester.ingest(master, master.replace("_master.h5", "_meta.h5"), str(dcid))
+
+    print("{0} GB read in {1}s".format(r / (1024.0 ** 3), t))
+
+
+if __name__ == "__main__":
+    import sys
+
+    master = sys.argv[1]
+    dcid = int(sys.argv[2])
+    ingest(master, dcid)
