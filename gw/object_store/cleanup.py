187ab7fcafcfa533377ca77c16f641272f0f3442
Graeme Winter <graeme.winter@gmail.com>
2020-04-15 20:58:31 +0100

    A cleanup script

---
+++ gw/object_store/cleanup.py
@@ -0,0 +1,27 @@
+import minio
+import sys
+
+
+class Cleaner(object):
+    def __init__(self, minio_server, username, password):
+        self._client = minio.Minio(
+            "localhost:9000", access_key=username, secret_key=password, secure=False
+        )
+
+    def clean(self, bucket):
+        """Remove every item from the bucket before removing the bucket"""
+
+        for item in self._client.list_objects(bucket):
+            self._client.remove_object(item.bucket_name, item.object_name)
+
+        self._client.remove_bucket(bucket)
+
+
+def cleanup(bucket):
+    cleaner = Cleaner("localhost:9000", "hello", "k1tty-pass")
+    cleaner.cleanup(bucket)
+
+
+if __name__ == "__main__":
+    dcid = int(sys.argv[1])
+    cleanup(dcid)
