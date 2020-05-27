55b55db432acc7a2329fcb50d1e288cf8a656e80
Nicholas Devenish <ndevenish@gmail.com>
2020-05-27 17:21:34 +0100

    object_store: Add a way to change host

--- gw/object_store/ingest.py
+++ gw/object_store/ingest.py
@@ -1,15 +1,25 @@
 import io
 import time
+import urllib.parse
 from argparse import ArgumentParser
 
 import h5py
 import minio
+from tqdm import tqdm
 
 
 class Ingester(object):
-    def __init__(self, minio_server, username, password):
+    def __init__(self, minio_server):
+        parts = urllib.parse.urlparse(minio_server)
+
+        host = f"{parts.hostname}:{parts.port}"
+        user = f"{parts.username}@" if parts.username else ""
+        print(f"Using {user}{host}")
         self._client = minio.Minio(
-            "localhost:9000", access_key=username, secret_key=password, secure=False
+            host,
+            access_key=parts.username or None,
+            secret_key=parts.password or None,
+            secure=False,
         )
 
     def ingest(self, master, meta, bucket):
@@ -38,34 +48,42 @@ class Ingester(object):
             total_read = 0
             t0 = time.time()
 
-            # assume all the "real" data in data_(something) datasets
-            for k in sorted(d for d in data if d.startswith("data_")):
-                d = data[k]
-                d_id = d.id
+            # Add up length of chunks
+            n_image_chunks = sum(
+                data[d].shape[0] for d in data if d.startswith("data_")
+            )
 
-                # getting the dataset shape should
-                chunks = d.shape[0]
+            with tqdm(total=n_image_chunks) as progress:
+                # assume all the "real" data in data_(something) datasets
+                for k in sorted(d for d in data if d.startswith("data_")):
+                    dataset = data[k]
+                    d_id = dataset.id
 
-                # iterate through the chunks found in here and push each up
-                for j in range(chunks):
-                    offset = (j, 0, 0)
-                    filter_mask, chunk = d_id.read_direct_chunk(offset)
-                    chunk_size = len(chunk)
+                    # getting the dataset shape should
+                    chunks = dataset.shape[0]
 
-                    # increment counter - we push this in people numbers
-                    frame_index += 1
+                    # iterate through the chunks found in here and push each up
+                    for j in range(chunks):
+                        offset = (j, 0, 0)
+                        filter_mask, chunk = d_id.read_direct_chunk(offset)
+                        chunk_size = len(chunk)
 
-                    # push chunk to object store
-                    etag = self._client.put_object(
-                        bucket, "%06d" % frame_index, io.BytesIO(chunk), chunk_size
-                    )
-                    total_read += chunk_size
+                        # increment counter - we push this in people numbers
+                        frame_index += 1
+
+                        # push chunk to object store
+                        self._client.put_object(
+                            bucket, "%06d" % frame_index, io.BytesIO(chunk), chunk_size
+                        )
+                        total_read += chunk_size
+                        progress.update(1)
+
         t1 = time.time()
         return total_read, t1 - t0
 
 
-def ingest(master, dcid):
-    ingester = Ingester("localhost:9000", "hello", "k1tty-pass")
+def ingest(host, master, dcid):
+    ingester = Ingester(host)
     r, t = ingester.ingest(master, master.replace("_master.h5", "_meta.h5"), str(dcid))
 
     print("{0} GB read in {1}s".format(r / (1024.0 ** 3), t))
@@ -75,5 +93,15 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="Loads h5 images to object store")
     parser.add_argument("filename", metavar="MASTER_FILE")
     parser.add_argument("dcid", metavar="DCID", type=int)
+    parser.add_argument(
+        "--host",
+        metavar="URL",
+        type=str,
+        help="Endpoint; user:pass@host:port",
+        default="minio://hello:k1tty-pass@localhost:9000",
+    )
     args = parser.parse_args()
-    ingest(args.filename, args.dcid)
+    # Try to be default-sensible
+    if not "://" in args.host:
+        args.host = f"minio://{args.host}"
+    ingest(args.host, args.filename, args.dcid)
