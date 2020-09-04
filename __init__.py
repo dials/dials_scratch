51b5081e1171c6420bcdb8c0b44bdff21b399828
Graeme Winter <graeme.winter@gmail.com>
2020-09-04 15:50:50 +0100

    += boost_adaptbx.

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 from __future__ import division
 
 try:
-    import boost.python
+    import boost_adaptbx.boost.python
 except Exception:
     ext = None
 else:
-    ext = boost.python.import_ext("dials_scratch_ext", optional=True)
+    ext = boost_adaptbx.boost.python.import_ext("dials_scratch_ext", optional=True)
 
 if not ext is None:
     from dials_scratch_ext import *
