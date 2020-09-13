8f165febd4cd8d10501eb7172b3eabb944b33ccc
DiamondLightSource-build-server <DiamondLightSource-build-server@users.noreply.github.com>
2020-09-13 05:10:53 +0100

    Clean clutter

--- command_line/match_identical_integration.py
+++ command_line/match_identical_integration.py
@@ -5,10 +5,10 @@ from annlib_ext import AnnAdaptorSelfInclude
 
 
 def match(a, b, max_separation=2, key="xyzobs.px.value", scale=(1, 1, 1)):
-    """Match reflections from list a and list b, returning tuple of 
-    flex.size_t indices, optionally specifying the maximum distance and 
+    """Match reflections from list a and list b, returning tuple of
+    flex.size_t indices, optionally specifying the maximum distance and
     key to search on (which is assumed to be a 3-vector column). Can also
-    apply relative scales to the vectors before matching in case e.g. very 
+    apply relative scales to the vectors before matching in case e.g. very
     wide or very fine slicing."""
 
     xyz_a = a[key]
@@ -44,7 +44,7 @@ def match(a, b, max_separation=2, key="xyzobs.px.value", scale=(1, 1, 1)):
 
 
 def compare(file1, file2):
-    """Match the reflections between file1, file2, extract intensities and 
+    """Match the reflections between file1, file2, extract intensities and
     compare."""
     data1 = flex.reflection_table.from_file(file1)
     data1 = data1.select(data1.get_flags(data1.flags.integrated))
