e455f9082a7f8e0f9be88b2d499682bc9494594f
DiamondLightSource-build-server <DiamondLightSource-build-server@users.noreply.github.com>
2021-06-06 05:11:19 +0100

    Clean clutter

--- command_line/ssx_index.py
+++ command_line/ssx_index.py
@@ -19,6 +19,7 @@ from dials.util.version import dials_version
 from dxtbx.model import ExperimentList
 from dials.array_family import flex
 from dials.algorithms.indexing.indexer import Indexer
+
 try:
     from typing import List
 except ImportError:
@@ -51,11 +52,13 @@ refinement {
 }
 """
 
-phil_scope = phil.parse("""
+phil_scope = phil.parse(
+    """
 include scope dials.command_line.index.phil_scope
-""", process_includes=True).fetch(
-    phil.parse(program_defaults_phil_str)
-)
+""",
+    process_includes=True,
+).fetch(phil.parse(program_defaults_phil_str))
+
 
 def index(experiments, observed, params):
     params.refinement.parameterisation.scan_varying = False
@@ -66,7 +69,7 @@ def index(experiments, observed, params):
     indexed_reflections = flex.reflection_table()
 
     def run_with_disabled_logs(fn, fnargs):
-        sys.stdout = open(os.devnull, 'w') # block printing from rstbx
+        sys.stdout = open(os.devnull, "w")  # block printing from rstbx
         log1 = logging.getLogger("dials.algorithms.refinement.reflection_manager")
         log2 = logging.getLogger("dials.algorithms.refinement.refiner")
         log3 = logging.getLogger("dials.algorithms.indexing.stills_indexer")
@@ -76,13 +79,15 @@ def index(experiments, observed, params):
                 with LoggingContext(log3, level=logging.WARNING):
                     with LoggingContext(log4, level=logging.WARNING):
                         fn(*fnargs)
-        sys.stdout = sys.__stdout__ # restore printing
+        sys.stdout = sys.__stdout__  # restore printing
 
     def index_all(experiments, reflections, params):
         n_found = 0
         for i, (expt, refl) in enumerate(zip(experiments, reflections)):
             elist = ExperimentList([expt])
-            refl["imageset_id"] = flex.int(refl.size(), 0) #needed for centroid_px_to_mm
+            refl["imageset_id"] = flex.int(
+                refl.size(), 0
+            )  # needed for centroid_px_to_mm
             refl.centroid_px_to_mm(elist)
             refl.map_centroids_to_reciprocal_space(elist)
             idxr = Indexer.from_parameters(
@@ -95,25 +100,31 @@ def index(experiments, observed, params):
             except DialsIndexError as e:
                 logger.info(f"Image {i+1}: Failed to index, error: {e}")
             else:
-                #renumber numerical ids in the table
+                # renumber numerical ids in the table
                 ids_map = dict(idxr.refined_reflections.experiment_identifiers())
                 for k in idxr.refined_reflections.experiment_identifiers().keys():
                     del idxr.refined_reflections.experiment_identifiers()[k]
                 idxr.refined_reflections["id"] += n_found
-                for k,v in ids_map.items():
-                    idxr.refined_reflections.experiment_identifiers()[k+n_found] = v
+                for k, v in ids_map.items():
+                    idxr.refined_reflections.experiment_identifiers()[k + n_found] = v
                 n_found += len(ids_map.keys())
-                logger.info(f"Image {i+1}: Indexed {idxr.refined_reflections.size()} spots")
+                logger.info(
+                    f"Image {i+1}: Indexed {idxr.refined_reflections.size()} spots"
+                )
                 indexed_reflections.extend(idxr.refined_reflections)
                 indexed_experiments.extend(idxr.refined_experiments)
 
-
     run_with_disabled_logs(index_all, (experiments, reflections, params))
-    logger.info(f"{indexed_reflections.size()} spots indexed across {len(indexed_experiments)} images")
+    logger.info(
+        f"{indexed_reflections.size()} spots indexed across {len(indexed_experiments)} images"
+    )
 
     from dials.command_line.combine_experiments import CombineWithReference
+
     # combine beam and detector models if not already
-    if (len(indexed_experiments.detectors())) > 1 or (len(indexed_experiments.beams())) > 1:
+    if (len(indexed_experiments.detectors())) > 1 or (
+        len(indexed_experiments.beams())
+    ) > 1:
         combine = CombineWithReference(
             detector=indexed_experiments[0].detector,
             beam=indexed_experiments[0].beam,
@@ -128,6 +139,7 @@ def index(experiments, observed, params):
     indexed_experiments.as_file(expt_filename)
     logger.info(f"Saving indexed reflections to {refl_filename}")
     indexed_reflections.as_file(refl_filename)
+
 
 @show_mail_handle_errors()
 def run(args: List[str] = None, phil: phil.scope = phil_scope) -> None:
@@ -153,6 +165,6 @@ def run(args: List[str] = None, phil: phil.scope = phil_scope) -> None:
 
     index(experiments, reflections[0], params)
 
+
 if __name__ == "__main__":
     run()
-
