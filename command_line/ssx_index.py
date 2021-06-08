62f36dee2cd96ac65492fe27cd1c7e5accf7b808
James Beilsten-Edmands <jbeilstenedmands@gmail.com>
2021-06-08 14:29:47 +0100

    Updates to print summary table

--- command_line/ssx_index.py
+++ command_line/ssx_index.py
@@ -8,11 +8,12 @@ with a joint detector and beam model.
 
 import logging
 import sys, os
+import math
 from io import StringIO
 
 from libtbx import phil
 
-from dials.util import log, show_mail_handle_errors
+from dials.util import log, show_mail_handle_errors, tabulate
 from dials.util.options import OptionParser, reflections_and_experiments_from_files
 from dials.util.version import dials_version
 
@@ -31,6 +32,7 @@ from dials.command_line.index import phil_scope
 from dials.algorithms.indexing import DialsIndexError
 from dials.util.log import LoggingContext
 
+RAD2DEG = 180 / math.pi
 
 program_defaults_phil_str = """
 indexing {
@@ -75,12 +77,14 @@ def index(experiments, observed, params):
         log3 = logging.getLogger("dials.algorithms.indexing.stills_indexer")
         log4 = logging.getLogger("dials.algorithms.indexing.nave_parameters")
         log5 = logging.getLogger("dials.algorithms.indexing.basis_vector_search.real_space_grid_search")
+        log6 = logging.getLogger("dials.algorithms.indexing.basis_vector_search.combinations")
         with LoggingContext(log1, level=logging.ERROR):
             with LoggingContext(log2, level=logging.ERROR):
                 with LoggingContext(log3, level=logging.ERROR):
                     with LoggingContext(log4, level=logging.ERROR):
                         with LoggingContext(log5, level=logging.ERROR):
-                            fn(*fnargs)
+                            with LoggingContext(log6, level=logging.ERROR):
+                                fn(*fnargs)
         sys.stdout = sys.__stdout__  # restore printing
 
     if params.indexing.known_symmetry.unit_cell:
@@ -96,13 +100,20 @@ def index(experiments, observed, params):
 
     def index_all(experiments, reflections, params):
         n_found = 0
+        overall_summary_header = ["Image", "lattice", "expt_id", "n_indexed", "dX", "dY", "dPsi"]
+        overall_summary_rows = []
+        # toggle on n_lattice and method depending on if multiple or not?
         for i, (expt, refl) in enumerate(zip(experiments, reflections)):
+            ### Calculate necessary quantities
             elist = ExperimentList([expt])
             refl["imageset_id"] = flex.int(
                 refl.size(), 0
             )  # needed for centroid_px_to_mm
             refl.centroid_px_to_mm(elist)
             refl.map_centroids_to_reciprocal_space(elist)
+            path = expt.imageset.get_path(i)
+
+            ### Loop over methods and attempt indexing
             for method in method_list:
                 params.indexing.method = method
                 idxr = Indexer.from_parameters(refl, elist, params=params)
@@ -111,17 +122,49 @@ def index(experiments, observed, params):
                 except DialsIndexError as e:
                     logger.info(f"Image {i+1}: Failed to index{plural(method)}, error: {e}")
                 else:
-                    # renumber numerical ids in the table
+                    ### Extract rmsd values for output
                     ids_map = dict(idxr.refined_reflections.experiment_identifiers())
+                    logger.info(f"Image {i+1}: Indexed with {method} method.")
+                    header = ["Crystal #", "n indexed", "RMSD_X (px)", "RMSD_Y (px)", "RMSD_DeltaPsi (deg)"]
+                    rows = []
+                    n_strong = refl.size()
+                    for n_cryst, id_ in enumerate(ids_map.keys()):
+                        selr = idxr.refined_reflections.select(idxr.refined_reflections["id"] == id_)
+                        calx, caly, calz = selr["xyzcal.px"].parts()
+                        obsx, obsy, obsz = selr["xyzobs.px.value"].parts()
+                        delpsi = selr["delpsical.rad"]
+                        rmsd_x = flex.mean((calx - obsx) ** 2)**0.5
+                        rmsd_y = flex.mean((caly - obsy) ** 2)**0.5
+                        rmsd_z = flex.mean(((delpsi) * RAD2DEG) ** 2)**0.5
+                        n_id_ = calx.size()
+                        n_indexed = f"{n_id_}/{n_strong} ({100*n_id_/n_strong:2.1f}%)"
+                        rows.append([
+                            f"{n_cryst + 1}",
+                            n_indexed,
+                            f"{rmsd_x:.3f}",
+                            f"{rmsd_y:.3f}",
+                            f" {rmsd_z:.4f}",
+                        ])
+                        overall_summary_rows.append([
+                            path.split("/")[-1],
+                            n_cryst + 1,
+                            n_found + n_cryst,
+                            n_indexed,
+                            f"{rmsd_x:.3f}",
+                            f"{rmsd_y:.3f}",
+                            f" {rmsd_z:.4f}",
+                        ])
+                    logger.info(tabulate(rows, header))
+
+                    ### renumber numerical ids in the table
                     for k in idxr.refined_reflections.experiment_identifiers().keys():
                         del idxr.refined_reflections.experiment_identifiers()[k]
                     idxr.refined_reflections["id"] += n_found
                     for k, v in ids_map.items():
                         idxr.refined_reflections.experiment_identifiers()[k + n_found] = v
                     n_found += len(ids_map.keys())
-                    logger.info(
-                        f"Image {i+1}: Indexed {idxr.refined_reflections.size()} spots{plural(method)}"
-                    )
+
+                    ### Add data to overall list/table
                     indexed_reflections.extend(idxr.refined_reflections)
                     for jexpt in idxr.refined_experiments:
                         indexed_experiments.append(
@@ -137,9 +180,14 @@ def index(experiments, observed, params):
                         )
                     break
 
+        # now give overall summary
+        logger.info(tabulate(overall_summary_rows, overall_summary_header))
+
+
     run_with_disabled_logs(index_all, (experiments, reflections, params))
+    n_images = len(set(e.imageset.get_path(0) for e in indexed_experiments))
     logger.info(
-        f"{indexed_reflections.size()} spots indexed across {len(indexed_experiments)} images"
+        f"{indexed_reflections.size()} spots indexed on {n_images} images"
     )
 
     from dials.command_line.combine_experiments import CombineWithReference
