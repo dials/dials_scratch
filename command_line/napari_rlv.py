710b61f97a55dcccdba3b68ee1603b7f2781685c
David Waterman <dgwaterman@gmail.com>
2022-04-13 16:29:11 +0100

    Experimental dev.dials.napari_rlv.
    Copy of DIALS reciprocal lattice viewer code, with wx and GL parts
    commented out. Basic attempt to reproduce the drawing in napari
    layers. Obviously still needs work.

---
+++ command_line/napari_rlv.py
@@ -0,0 +1,78 @@
+# LIBTBX_PRE_DISPATCHER_INCLUDE_SH export PHENIX_GUI_ENVIRONMENT=1
+# DIALS_ENABLE_COMMAND_LINE_COMPLETION
+# LIBTBX_SET_DISPATCHER_NAME dev.dials.napari_rlv
+
+from __future__ import annotations
+
+import copy
+import os
+
+import napari
+
+from scitbx.array_family import flex
+
+import dials.util.log
+from dials.util.options import ArgumentParser, reflections_and_experiments_from_files
+from dials_scratch.dgw.rlv.viewer import ReciprocalLatticeViewer, phil_scope
+
+help_message = """
+Visualise the strong spots from spotfinding in reciprocal space.
+
+Examples::
+
+  dev.dials.napari_rlv imported.expt strong.refl
+
+  dev.dials.napari_rlv indexed.expt indexed.refl
+"""
+
+
+@dials.util.show_mail_handle_errors()
+def run(args=None):
+    dials.util.log.print_banner()
+    usage = "dev.dials.napari_rlv [options] models.expt observations.refl"
+
+    parser = ArgumentParser(
+        usage=usage,
+        phil=phil_scope,
+        read_experiments=True,
+        read_reflections=True,
+        check_format=False,
+        epilog=help_message,
+    )
+
+    params, options = parser.parse_args(args, show_diff_phil=True)
+    reflections, experiments = reflections_and_experiments_from_files(
+        params.input.reflections, params.input.experiments
+    )
+
+    if len(experiments) == 0 or len(reflections) == 0:
+        parser.print_help()
+        exit(0)
+
+    if len(reflections) > 1:
+        assert len(reflections) == len(experiments)
+        for i in range(len(reflections)):
+            reflections[i]["imageset_id"] = flex.int(len(reflections[i]), i)
+            if i > 0:
+                reflections[0].extend(reflections[i])
+    elif "imageset_id" not in reflections[0]:
+        reflections[0]["imageset_id"] = reflections[0]["id"]
+
+    reflections = reflections[0]
+
+    viewer = napari.Viewer(ndisplay=3)
+    rlv = ReciprocalLatticeViewer(
+        None,
+        -1,
+        os.path.realpath(params.input.reflections[0].filename),
+        size=(1024, 768),
+        settings=copy.deepcopy(params),
+    )
+    rlv.load_models(experiments, reflections)
+    rlv.add_to_napari(viewer)
+
+    napari.run()
+
+
+if __name__ == "__main__":
+    run()
