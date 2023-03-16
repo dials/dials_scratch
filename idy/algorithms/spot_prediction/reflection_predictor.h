cdccf639b7276c663ae7980be94afdad02970620
David Waterman <dgwaterman@gmail.com>
2023-03-16 09:22:02 +0000

    Move include to the .cc file.
    I get the feeling this is due to poorly structured .h/.cc setup,
    bad include guards or something. Nevertheless, this makes the build
    work.

--- idy/algorithms/spot_prediction/reflection_predictor.h
+++ idy/algorithms/spot_prediction/reflection_predictor.h
@@ -10,7 +10,6 @@
 #define IDY_ALGORITHMS_SPOT_PREDICTION_REFLECTION_PREDICTOR_H
 
 #include <algorithm>
-#include <dials/array_family/reflection_table.h>
 #include <dials/algorithms/spot_prediction/ray_predictor.h>
 #include <dials/algorithms/spot_prediction/reeke_index_generator.h>
 #include <dials/algorithms/spot_prediction/reflection_predictor.h>
