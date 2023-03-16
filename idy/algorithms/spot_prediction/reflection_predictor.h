390b85f27bddd15b20240b2247146f4547884ef6
David Waterman <dgwaterman@gmail.com>
2023-03-16 09:13:14 +0000

    Include dials/array_family/reflection_table.h.

--- idy/algorithms/spot_prediction/reflection_predictor.h
+++ idy/algorithms/spot_prediction/reflection_predictor.h
@@ -13,6 +13,7 @@
 #include <dials/algorithms/spot_prediction/ray_predictor.h>
 #include <dials/algorithms/spot_prediction/reeke_index_generator.h>
 #include <dials/algorithms/spot_prediction/reflection_predictor.h>
+#include <dials/array_family/reflection_table.h>
 #include <dials_scratch/idy/algorithms/spot_prediction/stills_ray_predictor.h>
 #include <dxtbx/model/beam.h>
 #include <dxtbx/model/detector.h>
