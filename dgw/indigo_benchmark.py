b53119a6dc53b0f756e98d96e2130dd8f9820427
David Waterman <dgwaterman@gmail.com>
2024-06-21 13:57:52 +0100

    Extending to work with three example cases: - simulated ED images - TPB images from the ELDICO instrument - One of Lei Wang's P1 lysozyme examples from eBIC

--- dgw/indigo_benchmark.py
+++ dgw/indigo_benchmark.py
@@ -13,7 +13,7 @@ from dxtbx.model.experiment_list import ExperimentListFactory
 from dials.algorithms.indexing.compare_orientation_matrices import (
     difference_rotation_matrix_axis_angle,
 )
-from time import perf_counter_ns
+from time import perf_counter
 import json
 
 
@@ -22,6 +22,9 @@ class Script(object):
         self,
         prefix="noiseimage_",
         extension="img",
+        unit_cell="78.840,78.840,38.290,90.000,90.000,90.000",
+        space_group="P43212",
+        import_cmds=[],
         spotfind_cmds=[
             "min_spot_size=3",
         ],
@@ -29,6 +32,7 @@ class Script(object):
 
         self.prefix = prefix
         self.extension = extension
+        self.import_cmds = import_cmds
         self.spotfind_cmds = spotfind_cmds
 
         self.cmds = [
@@ -37,8 +41,8 @@ class Script(object):
                 "imported.expt",
                 "strong.refl",
                 "stills.indexer=sequences",
-                "unit_cell=78.840,78.840,38.290,90.000,90.000,90.000",
-                "space_group=P43212",
+                f"unit_cell={unit_cell}",
+                f"space_group={space_group}",
                 "n_macro_cycles=2",
                 "detector.fix=distance",
                 "indexing.method=real_space_grid_search",
@@ -48,8 +52,8 @@ class Script(object):
                 "imported.expt",
                 "strong.refl",
                 "stills.indexer=sequences",
-                "unit_cell=78.840,78.840,38.290,90.000,90.000,90.000",
-                "space_group=P43212",
+                f"unit_cell={unit_cell}",
+                f"space_group={space_group}",
                 "n_macro_cycles=2",
                 "detector.fix=distance",
                 "indexing.method=low_res_spot_match",
@@ -59,8 +63,8 @@ class Script(object):
                 "imported.expt",
                 "strong.refl",
                 "stills.indexer=sequences",
-                "unit_cell=78.840,78.840,38.290,90.000,90.000,90.000",
-                "space_group=P43212",
+                f"unit_cell={unit_cell}",
+                f"space_group={space_group}",
                 "n_macro_cycles=2",
                 "detector.fix=distance",
                 "bootstrap_crystal=True",
@@ -71,8 +75,8 @@ class Script(object):
                 "imported.expt",
                 "strong.refl",
                 "stills.indexer=sequences",
-                "unit_cell=78.840,78.840,38.290,90.000,90.000,90.000",
-                "space_group=P43212",
+                f"unit_cell={unit_cell}",
+                f"space_group={space_group}",
                 "n_macro_cycles=2",
                 "detector.fix=distance",
                 "indexing.method=pink_indexer",
@@ -80,6 +84,8 @@ class Script(object):
                 "min_lattices=5",
             ),  # pink_indexer
         ]
+        if os.path.isfile("restraint.phil"):
+            self.cmds = [cmd + ("restraint.phil",) for cmd in self.cmds]
 
         self.images = sorted(glob.glob(self.prefix + "*." + self.extension))
         print(
@@ -120,9 +126,8 @@ class Script(object):
         st = simple_table(rows, header)
         print(st.format())
 
-    @staticmethod
-    def compare_orientation_matrices(image):
-        serialno = image.lstrip(self.prefix).rstrip("." + self.extension)
+    def compare_orientation_matrices(self, image):
+        serialno = image.replace(self.prefix, "").replace("." + self.extension, "")
         exp1 = ExperimentListFactory.from_json_file("experiments_" + serialno + ".json")
         exp2 = ExperimentListFactory.from_json_file("indexed.expt")
         R_ij, axis, angle, cb_op = difference_rotation_matrix_axis_angle(
@@ -130,35 +135,77 @@ class Script(object):
         )
         return abs(angle)
 
+    def check_hkl(self, idx):
+        ground_truth = flex.reflection_table.from_file("ground_truth.refl")
+        nref = len(ground_truth)
+
+        # Select reflections indexed in both
+        sel = idx.get_flags(idx.flags.indexed) & ground_truth.get_flags(
+            ground_truth.flags.indexed
+        )
+        idx = idx.select(sel)
+        ground_truth = ground_truth.select(sel)
+
+        # Compare hkl
+        idx_hkl = idx["miller_index"].as_vec3_double()
+        gt_hkl = ground_truth["miller_index"].as_vec3_double()
+
+        # Count number of reflections indexed identically, and the number with
+        # inverted indices (indexed as the Friedel pairs).
+        n_direct = (idx_hkl - gt_hkl).norms().count(0.0)
+        n_inverse = (idx_hkl + gt_hkl).norms().count(0.0)
+
+        # Return the fraction of correctly indexed reflections
+        return max(n_direct, n_inverse) / nref
+
     def process(self, image):
         print(f"processing {image}")
-        cmd = (shutil.which("dials.import"), image)
+        cmd = (shutil.which("dials.import"), image) + tuple(self.import_cmds)
         result = subprocess.run(cmd)
-        cmd = (shutil.which("dials.find_spots"), "imported.expt") + self.spotfind_cmds
+        cmd = (shutil.which("dials.find_spots"), "imported.expt") + tuple(
+            self.spotfind_cmds
+        )
+        result = subprocess.run(cmd)
+        serialno = image.replace(self.prefix, "").replace("." + self.extension, "")
+        cmd = (
+            shutil.which("dials.index"),
+            f"experiments_{serialno}.json",
+            "strong.refl",
+            "detector.fix=distance",
+            "output.experiments=ground_truth.expt",
+            "output.reflections=ground_truth.refl",
+        )
+        if os.path.isfile("restraint.phil"):
+            cmd = cmd + ("restraint.phil",)
         result = subprocess.run(cmd)
         strong = flex.reflection_table.from_file("strong.refl")
-        d = {"nspots": len(strong)}
+        d = {"image": image, "nspots": len(strong)}
 
         nindexed = []
         offset_deg = []
         elapsed = []
+        correct_hkl = []
         for cmd in self.cmds:
-            start = perf_counter_ns()
+            start = perf_counter()
             result = subprocess.run(cmd)
-            elapsed.append(perf_counter_ns() - start)
+            elapsed.append(perf_counter() - start)
             if os.path.isfile("indexed.refl"):
                 idx = flex.reflection_table.from_file("indexed.refl")
                 nindexed.append(idx.get_flags(idx.flags.indexed).count(True))
                 offset_deg.append(self.compare_orientation_matrices(image))
+                correct_hkl.append(self.check_hkl(idx))
                 os.remove("indexed.refl")
                 os.remove("indexed.expt")
             else:
                 nindexed.append(0)
                 offset_deg.append(None)
+        os.remove("ground_truth.refl")
+        os.remove("ground_truth.expt")
 
         d["nindexed"] = nindexed
         d["offset_deg"] = offset_deg
         d["elapsed"] = elapsed
+        d["correct_hkl"] = correct_hkl
 
         print(
             "#Spots:{0} ".format(d["nspots"])
@@ -170,11 +217,34 @@ class Script(object):
 
 if __name__ == "__main__":
     # Edit prefix, extension and spotfinding commands as needed
+    # script = Script(
+    #    prefix="TPB_25032024_07_",
+    #    extension="cbf",
+    #    unit_cell="7.5840,11.225,19.711,90.0,90.0,90.0",
+    #    space_group="P222",
+    #    import_cmds = ["panel.gain=1.7", "convert_sequences_to_stills=True"],
+    #    spotfind_cmds=[
+    #        "d_max=10",
+    #    ],
+    # )
+    # script = Script(
+    #    prefix="as_cbf_",
+    #    extension="cbf",
+    #    unit_cell="25.909,30.9595,33.3054,88.229,71.351,67.848",
+    #    space_group="P1",
+    #    import_cmds=["convert_sequences_to_stills=True"],
+    #    spotfind_cmds=[
+    #        "d_max=10",
+    #    ],
+    # )
     script = Script(
         prefix="noiseimage_",
         extension="img",
+        unit_cell="78.840,78.840,38.290,90.000,90.000,90.000",
+        space_group="P43212",
+        import_cmds=[],
         spotfind_cmds=[
-            "min_spot_size=3",
+            "d_max=10",
         ],
     )
     script.run()
